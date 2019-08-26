fa9406b533bce1801864cc79cb917082b58e92ad
Achchuthan Yogarajah <achch1990@gmail.com>
2019-08-26 20:01:38 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!-- BLOCK#1 START DON'T CHANGE THE ORDER-->  
+<?php
+$title = "Home | SLGTI";
+ include_once("config.php"); 
+ include_once("head.php"); 
+ include_once("menu.php"); 
+ ?>
+<!--END DON'T CHANGE THE ORDER--> 
+
+<!--BLOCK#2 START YOUR CODER HERE -->
+    <h1>Hello, world!</h1>
+    <button type="button" class="btn btn-primary">Primary</button>
+<!--END OF YOUR COD-->
+
+<!--BLOCK#3 START DON'T CHANGE THE ORDER-->   
+<?php include_once("footer.php"); ?>
+<!--END DON'T CHANGE THE ORDER-->  
