9c9ae2027e1b84d5b890ce4f004f60ce7f6ae39d
achchuthany <achch1990@gmail.com>
2019-08-27 13:57:24 +0530

    update

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $title = "Home | SLGTI";
 <!--END DON'T CHANGE THE ORDER--> 
 
 <!--BLOCK#2 START YOUR CODE HERE -->
-    <h1>Hello, world!222344</h1>
+    <h1>Hello, world!54789</h1>
     <button type="button" class="btn btn-primary">Primary</button>
 <!--END OF YOUR COD-->
 
