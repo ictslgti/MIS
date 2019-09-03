4ba813d3eecdbe0f2552d84d48db69798125c9ec
midhushan <mithunanirudh@gmail.com>
2019-09-03 15:41:45 +0530

    some changes

--- examinations.php
+++ examinations.php
@@ -36,11 +36,13 @@ $title = "Department Details | SLGTI";
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/script.min.js"></script>
-
+<?php
+$module = "database";
+?>
     <form>
   <div class="form-row">
     <div class="form-group col-md-6">
-      <label for="inputEmail4">Module1</label>
+      <label class="control-label"  for="inputEmail4">amh</label>
       <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
     </div>
     <div class="form-group col-md-6">
@@ -59,41 +61,12 @@ $title = "Department Details | SLGTI";
       </div>
     </div>
     
-  <div class="form-group">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
-      <input type="text" class="form-control" id="inputCity">
-    </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">State</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
-    </div>
-    <div class="form-group col-md-2">
-      <label for="inputZip">Zip</label>
-      <input type="text" class="form-control" id="inputZip">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
-</form>
+ 
+ 
+
+ 
+</form><br>
+
 </body>
 
 </html>
