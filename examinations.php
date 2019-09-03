c99b820d91728f0b69cb86d9f2fc3c95e91220a3
midhushan <mithunanirudh@gmail.com>
2019-09-03 12:08:56 +0530

    new

--- examinations.php
+++ examinations.php
@@ -6,7 +6,37 @@ $title = "Department Details | SLGTI";
  ?>
 <!--END DON'T CHANGE THE ORDER-->
 
-<!--BLOCK#2 START YOUR CODER HERE -->
+<!--BLOCK#2 START MY CODER HERE -->
+<?php
+$module = "Database";
+?>
+<head>
+<style>
+h1{
+    text-align: center;
+}
+form{
+    text-align: center;
+
+}
+</style>
+</head>
+<h1>Examinations Portal</h1>
+<section class="container">
+<form>
+  <div class="form-group">
+    <label for="formGroupExampleInput">Student Index Number :</label>
+    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Student index number">
+  </div><br>
+  <div class="form-group">
+    <label for="formGroupExampleInput2">module</label>
+    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="module marks">
+  </div>
+</form>
+</section>
+
+
+<!-- END OF MY CODE -->
 
 
 
