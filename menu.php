d054c3638c88fa8aaa9de82247facd91e7e17f88
HOMEPC-ACHCHUTH\ACHCHUTHAN <achch1990@gmail.com>
2019-09-04 21:28:01 +0530

    menu items

--- menu.php
+++ menu.php
@@ -1,42 +1,86 @@
 <header class="container">
-      <!-- Fixed navbar -->
-      <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
-        <a class="navbar-brand" href="#"> <img src="img/logo.png" height="30" class="d-inline-block align-top"alt="" ></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+        <a class="navbar-brand" href="index.php"> <img src="img/logo.png" height="30" class="d-inline-block align-top"
+                alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Dashboard <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="library.php">Library</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="Food.php">Canteen</a>
-            </li>
-          </ul>
-          <ul class="navbar-nav px-3">
-          <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-         John Smith
-         <img src="img/user.png" class="userpicture" role="presentation" aria-hidden="true" width="35" height="35">
-
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Profile</a>
-        
-          <a class="dropdown-item" href="#">Signout</a>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php"> <i class="fas fa-home"></i> Dashboard <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="Department" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-university"></i> Department
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="Department">
+                        <a class="dropdown-item" href="#">Course</a>
+                        <a class="dropdown-item" href="#">Academic Year</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="Staff" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-user-tie"></i> Staff
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="Staff">
+                        <a class="dropdown-item" href="#">Leave Request</a>
+                        <a class="dropdown-item" href="#">Academic Year</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="Student" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-user-graduate"></i> Student
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="Student">
+                        <a class="dropdown-item" href="#">On-Peak/Off-Peak Request</a>
+                        <a class="dropdown-item" href="#">Academic Year</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-chalkboard-teacher"></i> Assessment</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="library.php"><i class="far fa-file-alt"></i> Inventory</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="library.php"> <i class="fas fa-book-open"></i> Library</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Food.php"> <i class="fas fa-hamburger"></i> Canteen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fab fa-facebook-messenger"></i> Messaging</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav px-3">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        John Smith
+                        <img src="img/user.png" class="userpicture" role="presentation" aria-hidden="true" width="35"
+                            height="35">
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Profile</a>
+                        <a class="dropdown-item" href="signin.php">Signout</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                </li>
+            </ul>
         </div>
-      </li>
-      <li class="nav-item">
-            </li>
-          </ul>
-          </div>
         </div>
-      </nav>
-    </header>
+    </nav>
+</header>
