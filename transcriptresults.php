6a172f650551e39a34e51eb6cc1da5f5476cf861
thurgesanselva <thurgesanselva@gmail.com>
2019-09-04 16:02:08 +0530

    new

---
+++ transcriptresults.php
@@ -0,0 +1,10 @@
+<?php
+$title = "Department Details | SLGTI";
+ include_once("config.php"); 
+ include_once("head.php"); 
+ include_once("menu.php"); 
+ ?>
+
+
+<!--BLOCK#3 START DON'T CHANGE THE ORDER-->
+<?php include_once("footer.php"); ?>
