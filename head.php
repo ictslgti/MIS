7f97f24a18208c1ea5905b4a8e838512cbaf7cb2
HOMEPC-ACHCHUTH\ACHCHUTHAN <achch1990@gmail.com>
2019-09-05 20:48:16 +0530

    faviocn

--- head.php
+++ head.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/signin.css">
