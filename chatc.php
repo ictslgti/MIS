d60a4a06d2784b8049e4ccc0d04b492e4af8c4fe
sahangafoor <sahangafoor@gmail.com>
2019-09-06 08:08:15 +0400

    addd

--- chatc.php
+++ chatc.php
@@ -10,63 +10,6 @@ include_once("menu.php");
 <!-- BLOCK#2 START YOUR CODER HERE -->
 
 
-     
-      
-      
-      
-    <!-- <button type="submit" formaction="academic.php" value="Add" name="Add"  class="btn btn-link">Add</button> </td>                            -->
-    
-      
-     
-      <!-- <td><button type="button" class="btn btn-link">Add</button> </td>  -->
-    
-    
-     
-    <div class="wrapper">
-            <!-- Sidebar Holder -->
-            <nav id="sidebar">
-                <div class="sidebar-header">
-                    <h3>Group Chat</h3>
-                </div>
-
-                <ul class="list-unstyled components">
-                    <p>GSN</p>
-                    <li class="active">
-                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">Home</a>
-                        <ul class="collapse list-unstyled" id="homeSubmenu">
-                            <li><a href="#">Home 1</a></li>
-                            <li><a href="#">Home 2</a></li>
-                            <li><a href="#">Home 3</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#">About</a>
-                        <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false">Pages</a>
-                        <ul class="collapse list-unstyled" id="pageSubmenu">
-                            <li><a href="#">Page 1</a></li>
-                            <li><a href="#">Page 2</a></li>
-                            <li><a href="#">Page 3</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#">Portfolio</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
-
-                <ul class="list-unstyled CTAs">
-                    <li><a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a></li>
-                    <li><a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a></li>
-                </ul>
-      <!-- <td><button type="button" class="btn btn-link">Add</button> </td>  -->
-    
-   
-  </tbody>
-  </form>
-</table>
-
 <!-- END YOUR CODER HERE -->
 
     <!-- BLOCK#3 START DON'T CHANGE THE ORDER -->
