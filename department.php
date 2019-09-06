f554d00ac1ef1313d32da6fe389c1c6fbdc149bf
abdullahart1991 <artabdullah31@gmail.com>
2019-09-06 11:21:14 +0530

    file update

---
+++ department.php
@@ -0,0 +1,129 @@
+<!-- BLOCK#1 START DON'T CHANGE THE ORDER -->
+<?php 
+$title = "Department Details | SLGTI" ;
+include_once("config.php"); 
+include_once("head.php"); 
+include_once("menu.php");
+
+ ?>
+<!-- END DON'T CHANGE THE ORDER -->
+
+<!-- BLOCK#2 START YOUR CODER HERE -->
+
+<h1>Department Details</h1>
+<table class="table table-hover">
+  <thead class="thead-dark">
+ 
+    <tr >
+
+
+     
+      <th scope="col">Department_ID</th>
+      <th scope="col">Department Name</th>
+      <th scope="col">Option</th>
+    </tr>
+  </thead>
+  <tbody>
+ 
+  <tr class="table-light">
+      
+     
+      <td>ICT/001</td>
+      <td>Information & Communications Technology Department</td>
+      <td><div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+  <div class="btn-group mr-2" role="group" aria-label="First group">
+  <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn btn-secondary">Courses</button>
+    <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn btn-secondary">Batches</button></td>
+     
+  
+  
+      <!-- <td><button type="button" class="btn btn-light dropdown-toggle" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20"> -->
+      
+      <!-- <div class="dropdown-menu" aria-labelledby="dropdownMenuOffset"> -->
+       <!-- <a class="dropdown-item" href="#">Course</a> -->
+      <!-- <a class="dropdown-item" href="#">Batches</a> -->
+      
+    <!-- </div> -->
+    <!-- <button type="submit" formaction="academic.php" value="Add" name="Add"  class="btn btn-link">Add</button> </td>                            -->
+    </tr>
+    <tr class="table-light">
+     
+     
+      <td>MT/002</td>
+      <td>Mechanical Technology Department</td>
+      <td><div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+  <div class="btn-group mr-2" role="group" aria-label="First group">
+  <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn btn-secondary">Courses</button>
+    <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn btn-secondary">Batches</button></td>
+      
+
+      <!-- <td><button type="button" class="btn btn-link">Add</button> </td>  -->
+    </tr>
+    <tr class="table-light">
+    
+     
+      <td>EET/003</td>
+      <td>Electrical & Electronic Technology Department</td>
+      <td><div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+  <div class="btn-group mr-2" role="group" aria-label="First group">
+  <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn btn-secondary">Courses</button>
+    <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn btn-secondary">Batches</button></td>
+      <!-- <td><button type="button" class="btn btn-link">Add</button> </td>  -->
+    </tr>
+    <tr class="table-light ">
+     
+     
+      <td>FT/004</td>
+      <td>Food Technology Department</td>
+      <td><div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+  <div class="btn-group mr-2" role="group" aria-label="First group">
+  <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn btn-secondary">Courses</button>
+    <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn btn-secondary">Batches</button></td>
+      <!-- <td><button type="button" class="btn btn-link">Add</button> </td>  -->
+    </tr>
+    <tr class="table-light">
+    
+     
+      <td>AAT/005</td>
+      <td>Automotive & Agricultural Technology Department</td>
+      <td><div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+  <div class="btn-group mr-2" role="group" aria-label="First group">
+  <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn btn-secondary">Courses</button>
+    <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn btn-secondary">Batches</button></td>
+      <!-- <td><button type="button" class="btn btn-link">Add</button> </td>  -->
+    </tr>
+    <tr class="table-light">
+     
+     
+      <td>CT/006</td>
+      <td>Construction Technology Department</td>
+      <td><div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+  <div class="btn-group mr-2" role="group" aria-label="First group">
+  <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn btn-secondary">Courses</button>
+    <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn btn-secondary">Batches</button></td>
+      <!-- <td><button type="button" class="btn btn-link">Add</button> </td>  -->
+    </tr>
+   
+  </tbody>
+  </form>
+</table>
+
+<!-- END YOUR CODER HERE -->
+
+    <!-- BLOCK#3 START DON'T CHANGE THE ORDER -->
+    <?php 
+    include_once("footer.php");
+    ?>
+    <!-- END DON'T CHANGE THE ORDER -->
