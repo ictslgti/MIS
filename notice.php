0edf6244da1bfff132e773eea44a4ab8366863a0
pirujeyam1995 <pirujeyam1995@gmail.com>
2019-09-06 21:08:54 +0530

    view_notice“

---
+++ notice.php
@@ -0,0 +1,106 @@
+<!-- BLOCK#1 START DON'T CHANGE THE ORDER-->
+<?php
+$title = "Home | SLGTI";
+include_once("config.php");
+include_once("head.php");
+include_once("menu.php");
+?>
+<!--END DON'T CHANGE THE ORDER-->
+
+<!--BLOCK#2 START YOUR CODE HERE -->
+<table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col"> <div class="card text-white bg-primary mb-3" style="max-width: 23rem;">
+                            <div class="card-header">Exam</div>
+                             <div class="dropdown"></div>
+                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                 aria-expanded="false"> Information & Communication Technology </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                  <a class="dropdown-item" href="#">Bridging</a>
+                                  <a class="dropdown-item" href="#">Level-5</a>
+                                  </div>
+                                <br> <br>
+                                <div class="dropdown"> </div>
+                                  <button class="btn btn-secondary dropdown-toggle" type="button" id="Button" data-toggle="dropdown" aria-haspopup="true"
+                                 aria-expanded="false"> Food Technology </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                 <a class="dropdown-item" href="#">Level-5</a>
+                                  </div>
+                               
+                                <br> <br>
+                                <div class="dropdown"></div>
+                                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                  aria-expanded="false">Construction Technology </button>
+                                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                  <a class="dropdown-item" href="#">Level-4</a>
+                                  <a class="dropdown-item" href="#">Level-5</a>
+                                  <a class="dropdown-item" href="#">Bridging</a>
+                                   </div>
+                                   <br> <br>
+                                   <div class="dropdown"></div>
+                                   <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                  aria-expanded="false"> Elrctrical & Electronic Technology </button>
+                                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                  <a class="dropdown-item" href="#">Level-4</a>
+                                  <a class="dropdown-item" href="#">Level-5</a>
+                                  <a class="dropdown-item" href="#">Bridging</a>
+                                   </div></th>
+
+                                   <th scope="col"> <div class="card text-white bg-primary mb-3" style="max-width: 23rem;">
+                                    <div class="card-header">Result</div>
+                                     <div class="dropdown"></div>
+                                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                         aria-expanded="false"> Information & Communication Technology </button>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                          <a class="dropdown-item" href="#">Bridging</a>
+                                          <a class="dropdown-item" href="#">Level-5</a>
+                                          </div>
+                                        <br> <br>
+                                        <div class="dropdown"> </div>
+                                          <button class="btn btn-secondary dropdown-toggle" type="button" id="Button" data-toggle="dropdown" aria-haspopup="true"
+                                         aria-expanded="false"> Food Technology </button>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                         <a class="dropdown-item" href="#">Level-5</a>
+                                          </div>
+                                       
+                                        <br> <br>
+                                        <div class="dropdown"></div>
+                                          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                          aria-expanded="false">Construction Technology </button>
+                                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                          <a class="dropdown-item" href="#">Level-4</a>
+                                          <a class="dropdown-item" href="#">Level-5</a>
+                                          <a class="dropdown-item" href="#">Bridging</a>
+                                           </div>
+                                           <br> <br>
+                                           <div class="dropdown"></div>
+                                           <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                          aria-expanded="false"> Elrctrical & Electronic Technology </button>
+                                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                          <a class="dropdown-item" href="#">Level-4</a>
+                                          <a class="dropdown-item" href="#">Level-5</a>
+                                          <a class="dropdown-item" href="#">Bridging</a>
+                                           </div></th>
+                      </tr>
+
+                      <tr>  <th scope="col"> <div class="card text-white bg-primary mb-3" style="max-width: 23rem;">
+                        <div class="card-header">Events</div>
+                         <div class="list-group">
+                                <a href="#" class="list-group-item list-group-item-action list-group-item-primary">Awading Ceremoney</a>
+                                 <a href="#" class="list-group-item list-group-item-action list-group-item-danger">Blood Donation</a>
+                                <a href="#" class="list-group-item list-group-item-action list-group-item-secondary">Christamas Day</a>
+                                <a href="#" class="list-group-item list-group-item-action list-group-item-success">Hons Minister Visit</a>
+                                
+                              </div>
+                    </thead>
+               
+
+                      </div>
+                    </div>
+                 
+<!--END OF YOUR COD-->
+
+<!--BLOCK#3 START DON'T CHANGE THE ORDER-->
+<?php include_once("footer.php"); ?>
+<!--END DON'T CHANGE THE ORDER-->
