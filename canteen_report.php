e6a40c24500b80d1fa507ed352f646935ad7c7d3
faheeeeeem <abdulwalith96@gmail.com>
2019-09-06 12:07:38 +0530

    updated

---
+++ canteen_report.php
@@ -0,0 +1,57 @@
+<!--block 1 start dont change the order-->
+
+<?php 
+$title="Daily report  | SLGTI";
+include_once("config.php");
+include_once("head.php");
+include_once("menu.php");
+?>
+
+<div>
+
+<table class="table table-hover">
+  <thead class="thead-dark">  
+    <tr >
+      <th scope="col">#</th>
+
+      <th scope="col">Date</th>
+      <th scope="col">Item ID</th>
+      <th scope="col">Quantity</th>
+  
+      <th scope="col">Total Amount</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr class= "table-light">
+
+      
+    </tr>
+    <tr class= "table-light">
+     
+      
+    </tr>
+    <tr class= "table-light">
+ 
+      
+</tr>
+<tr class= "table-light">
+  
+  </tr>
+  
+    <tr class= "table-light">
+
+     </tr>
+  
+  
+  </tbody>
+</table>
+
+
+
+<!--block 3 start dont change the order-->
+
+    <?php include_once("footer.php"); ?>
+
+    
+    <!-- end dont change the order-->
