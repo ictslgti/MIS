a5c7abb76950f8cc52011aa5f74868f44d8109e7
saruyansaru02 <saruyansaru02@gmail.com>
2019-09-06 11:43:12 +0530

    add new

---
+++ inventory_view table.php
@@ -0,0 +1,45 @@
+<?php
+$title = "Department Details | SLGTI";
+ include_once("config.php"); 
+ include_once("head.php"); 
+ include_once("menu.php"); 
+ ?>
+<!-- end default code -->
+
+<!-- start my code -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- end my code\ -->
+
+
+<!--BLOCK#3 START DON'T CHANGE THE ORDER-->
+<?php include_once("footer.php"); ?>
