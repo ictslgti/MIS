295fcebad60c1d3dadd4432d25ac29b6ea0c0e7c
saruyansaru02 <saruyansaru02@gmail.com>
2019-09-06 12:00:10 +0530

    addd

--- inventory_view table.php
+++ inventory_view table.php
@@ -7,10 +7,50 @@ $title = "Department Details | SLGTI";
 <!-- end default code -->
 
 <!-- start my code -->
+<form>
+<div class="w-100"></div>
+                        <div class="col form-group ml-5 mr-5 mt-3 container">
+                            <input type="text" class="form-control" id="search" aria-describedby="Department ID" placeholder="search">
+                            <button type="submit" class="btn btn-success ml-2 mt-3 float-right">update </button>
+                            <small id="bookNameHelp" class="form-text text-muted"></small> 
+        <div class="container" style=" margin-top: 10%; border: 2px solid 	#ADD8E6; border-radius: 8px;"> 
+            <div class="row">
+
+            
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Department Id</th>
+      <th scope="col">Item Id</th>
+      <th scope="col">Qunatity</th>
+      <th scope="col">sataus</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
 
 
 
-
+</form>
 
 
 
