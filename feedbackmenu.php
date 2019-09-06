b120cedb9b00c5e60415283763cb3c7d7cdb6272
marian8863 <marian8863@gmail.com>
2019-09-06 12:24:56 +0530

    menu“

---
+++ feedbackmenu.php
@@ -0,0 +1,105 @@
+<!-- BLOCK#1 START DON'T CHANGE THE ORDER-->
+<?php
+$title = "Home | SLGTI";
+include_once("config.php");
+include_once("head.php");
+include_once("menu.php");
+?>
+<!--END DON'T CHANGE THE ORDER-->
+
+<!--BLOCK#2 START YOUR CODE HERE -->
+<!--<div class="container">
+  <div class="row">
+    <div class="col">
+      
+    </div>
+    
+  </div>
+</div>-->
+
+<div class="container" >
+
+            <div  class="row" style="background-color:#37b6ff  ;padding:20px; border-top-right-radius: 20px ;border-top-left-radius: 20px ;">
+                <div class="col" style=" ">
+               
+                </div>
+            </div>
+
+  <div class="row justify-content-md-center">
+    <div class="col col-lg-2">
+     
+    </div>
+    <div class="col-md-auto">
+    <img src="img/feedbackmenu.png" alt="feedback">
+    </div>
+    <div class="col col-lg-2">
+   
+    </div>
+  </div>
+    <div class="row">
+   
+        <div class="col-md-auto">
+        <img src="img/feedbackmenu1.png" alt="feedback">
+        </div>
+        <div class="col">
+
+                     
+                   
+                            <p style="font-size:30px ;color:black">Student Feedback</p>
+                            <p style="font-size:20px">Evaluation of Teaching and Learning at SLGTI </p>
+
+                        SLGTI is committed to the highest standards of educational and other provision for its students, and encourages 
+                        students to provide the Institute with thoughtful and constructive feedback. Formal evaluations, together with informal 
+                        comments and consultations, are used to make improvements to our course and other provisions, and to provide encouragement to 
+                        staff where appropriate. 
+
+                        <br><br>
+                        Our learning from student feedback is directed at providing:<br>
+                        (a)    a safe, professional and friendly learning environment;<br>
+                        (b)    high quality teaching, assessment and management of learning;<br>
+                        (c)    regular and reliable feedback on student progress and achievements;<br>
+                        (d)    mechanisms for students to pursue grievances and learning related issues as required. 
+                    
+           
+         
+                    
+           
+        </div>
+        <div class="col-sm-auto">
+        <img src="img/feedbackmenu2.png" alt="feedback">
+        </div>
+    </div>
+    <!--feedbackmenu3-->
+            <div class="row">
+                    <div class="col">
+                     
+                    </div>
+                     <div class="col order-12">
+                           
+                    </div>
+                    <div class="col order-1">
+                    <a href="feedbacksummery"><img src="img/feedbackmenu3.png" alt="feedback"> </a>
+                    </div>
+            </div>
+
+            <div class="row">
+                <div class="col-auto mr-auto"><img src="img/feedbackmenu7.png" alt="feedback"></div>
+                <div class="col-auto"><img src="img/feedbackmenu6.png" alt="feedback"></div>
+            </div>
+
+
+            <div  class="row" style="background-color:#37b6ff  ;padding:20px; border-bottom-right-radius: 20px ;border-bottom-left-radius: 20px ;">
+                <div class="col" style=" ">
+               
+                </div>
+            </div>
+
+</div>
+
+
+
+<!--END OF YOUR COD-->
+
+<!--BLOCK#3 START DON'T CHANGE THE ORDER-->
+<?php include_once("footer.php"); ?>
+<!--END DON'T CHANGE THE ORDER-->
