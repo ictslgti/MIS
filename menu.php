4f2ec30ad44710b305c8ff70aee080a112c01513
HOMEPC-ACHCHUTH\ACHCHUTHAN <achch1990@gmail.com>
2019-09-09 21:18:50 +0530

    new menu

--- menu.php
+++ menu.php
@@ -1,68 +1,126 @@
  <!-- Sidebar -->
  <div class="border-right" id="sidebar-wrapper">
-     <div class="sidebar-heading">  <a href="index.php"> <img src="img/logo-1.png" height="30" class="d-inline-block align-top"
-                alt=""></a>       
-    </div>
-     <div class="list-group list-group-flush">    
+     <div class="sidebar-heading"> <a href="index.php"> <img src="img/logo-1.png" height="30"
+                 class="d-inline-block align-top" alt=""></a>
+     </div>
+     <div class="list-group list-group-flush">
          <a class="list-group-item list-group-item-action" href="index.php"> <i class="fas fa-home"></i>
              Dashboard <span class="sr-only">(current)</span></a>
 
          <div class="dropdown">
-             <a class="list-group-item list-group-item-action dropdown-toggle" href="#" id="Department"
-                 role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+             <a class="list-group-item list-group-item-action dropdown-toggle" href="#" id="Department" role="button"
+                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                  <i class="fas fa-university"></i> Department
              </a>
              <div class="dropdown-menu " aria-labelledby="Department">
-                 <a class="dropdown-item" href="#">Course</a>
-                 <a class="dropdown-item" href="#">Academic Year</a>
+                 <a class="dropdown-item" href="#">Department Info</a>
+                 <a class="dropdown-item" href="#">Add a Department</a>
                  <div class="dropdown-divider"></div>
-                 <a class="dropdown-item" href="#">Something else here</a>
+                 <a class="dropdown-item" href="#">Academic Year Info</a>
+                 <a class="dropdown-item" href="#">Add a Academic Year</a>
+                 <div class="dropdown-divider"></div>
+                 <a class="dropdown-item" href="#">Course Info</a>
+                 <a class="dropdown-item" href="#">Add a Course</a>
+                 <div class="dropdown-divider"></div>
+                 <a class="dropdown-item" href="#">Module Info</a>
+                 <a class="dropdown-item" href="#">Add a Module</a>
              </div>
-        </div>
-        <div class="dropdown">
-             <a class="list-group-item list-group-item-action dropdown-toggle" href="#" id="Staff" role="button" data-toggle="dropdown"
-                 aria-haspopup="true" aria-expanded="false">
+         </div>
+         <div class="dropdown">
+             <a class="list-group-item list-group-item-action dropdown-toggle" href="#" id="Staff" role="button"
+                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                  <i class="fas fa-user-tie"></i> Staff
              </a>
              <div class="dropdown-menu" aria-labelledby="Staff">
-                 <a class="dropdown-item" href="#">Leave Request</a>
-                 <a class="dropdown-item" href="#">Academic Year</a>
+                 <a class="dropdown-item" href="staffdetails.php">Staff Info</a>
+                 <a class="dropdown-item" href="#">Add a Staff</a>
                  <div class="dropdown-divider"></div>
-                 <a class="dropdown-item" href="staffdetails.php">staffdetails</a>
+                 <a class="dropdown-item" href="#">Module Enrollment</a>
+                 <div class="dropdown-divider"></div>
+                 <a class="dropdown-item" href="staffdetails.php">Staff Exit</a>
              </div>
-        </div>
-        <div class="dropdown">
-             <a class="list-group-item list-group-item-action dropdown-toggle" href="#" id="Student" role="button" data-toggle="dropdown"
-                 aria-haspopup="true" aria-expanded="false">
+         </div>
+         <div class="dropdown">
+             <a class="list-group-item list-group-item-action dropdown-toggle" href="#" id="Student" role="button"
+                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                  <i class="fas fa-user-graduate"></i> Student
              </a>
              <div class="dropdown-menu" aria-labelledby="Student">
-                 <a class="dropdown-item" href="#">On-Peak/Off-Peak Request</a>
-                 <a class="dropdown-item" href="#">Academic Year</a>
+                 <a class="dropdown-item" href="#">Student Info</a>
+                 <a class="dropdown-item" href="#">Add a Student</a>
                  <div class="dropdown-divider"></div>
-                 <a class="dropdown-item" href="#">Something else here</a>
+                 <a class="dropdown-item" href="#">Studen Re Enroll</a>
+                 <div class="dropdown-divider"></div>
+                 <a class="dropdown-item" href="#">Student Enrollment Report</a>
              </div>
-        </div>
-        <div class="dropdown">
-             <a class="list-group-item list-group-item-action dropdown-toggle" href="#" id="ojt" role="button" data-toggle="dropdown"
-                 aria-haspopup="true" aria-expanded="false">
-                 <i class="fas fa-briefcase"></i> OJT
+         </div>
+         <div class="dropdown">
+             <a class="list-group-item list-group-item-action dropdown-toggle" href="#" id="Assessment" role="button"
+                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                 <i class="fas fa-chalkboard-teacher"></i> Examination
+             </a>
+             <div class="dropdown-menu" aria-labelledby="Assessment">
+                 <a class="dropdown-item" href="#">Assessment Info</a>
+                 <a class="dropdown-item" href="#">Add a Assessment</a>
+                 <div class="dropdown-divider"></div>
+                 <a class="dropdown-item" href="#">Assessment Report</a>
+                 <div class="dropdown-divider"></div>
+                 <a class="dropdown-item" href="#">TVEC Examination Info</a>
+             </div>
+         </div>
+         <div class="dropdown">
+             <a class="list-group-item list-group-item-action dropdown-toggle" href="#" id="Assessment" role="button"
+                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                 <i class="fas fa-chalkboard-teacher"></i> Attendance
+             </a>
+             <div class="dropdown-menu" aria-labelledby="Assessment">
+                 <a class="dropdown-item" href="#">Attendance Info</a>
+                 <a class="dropdown-item" href="#">Add a Attendance</a>
+                 <div class="dropdown-divider"></div>
+                 <a class="dropdown-item" href="#">Attendance Report</a>
+                 <div class="dropdown-divider"></div>
+                 <a class="dropdown-item" href="#">Warnings Letters</a>
+             </div>
+         </div>
+         <div class="dropdown">
+             <a class="list-group-item list-group-item-action dropdown-toggle" href="#" id="ojt" role="button"
+                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                 <i class="fas fa-briefcase"></i> On-the-job Training
              </a>
              <div class="dropdown-menu" aria-labelledby="ojt">
-                 <a class="dropdown-item" href="#">Requesting to a place</a>
-                 <a class="dropdown-item" href="#">view your training place</a>
-                 <a class="dropdown-item" href="#">Changing request to your training place</a>
+                 <a class="dropdown-item" href="#">On-the-job Training Info</a>
+                 <a class="dropdown-item" href="#">Add a Training Place</a>
+                 <a class="dropdown-item" href="#">Students Request</a>
                  <div class="dropdown-divider"></div>
-                 <a class="dropdown-item" href="#">Something else here</a>
+                 <a class="dropdown-item" href="#">Placement Request</a>
+                 <a class="dropdown-item" href="#">Training Place Info </a>
+                 <a class="dropdown-item" href="#">Placement Chage</a>
+                 <div class="dropdown-divider"></div>
+                 <a class="dropdown-item" href="#">OJT Report</a>
              </div>
-        </div>
-             <a class="list-group-item list-group-item-action" href="examinations.php"><i class="fas fa-chalkboard-teacher"></i> Assessment</a>
-
-             <a class="list-group-item list-group-item-action" href="library.php"><i class="far fa-file-alt"></i> Inventory</a>
-        <div class="dropdown">
-             <a class="list-group-item list-group-item-action dropdown-toggle" href="#" id="library" role="button" data-toggle="dropdown"
-                 aria-haspopup="true" aria-expanded="false">
-                 <i class="fas fa-book-open"></i> Library 
+         </div>
+         <div class="dropdown">
+             <a class="list-group-item list-group-item-action dropdown-toggle" href="#" id="library" role="button"
+                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                 <i class="far fa-grin"></i> Feedback
+             </a>
+             <div class="dropdown-menu" aria-labelledby="library">
+                 <a class="dropdown-item" href="feedbacksummery.php">Student Feedback Info</a>
+                 <a class="dropdown-item" href="feedback.php">Create a Student Feedback</a>
+                 <div class="dropdown-divider"></div>
+                 <a class="dropdown-item" href="#">Teacher Feedback Info</a>
+                 <a class="dropdown-item" href="#">Create a Teacher Feedback</a>
+                 <div class="dropdown-divider"></div>
+                 <a class="dropdown-item" href="#">Industry Feedback Info</a>
+                 <a class="dropdown-item" href="#">Create a Industry Feedback</a>
+             </div>
+         </div>
+         <a class="list-group-item list-group-item-action" href="library.php"><i class="far fa-file-alt"></i>
+             Inventory</a>
+         <div class="dropdown">
+             <a class="list-group-item list-group-item-action dropdown-toggle" href="#" id="library" role="button"
+                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                 <i class="fas fa-book-open"></i> Library
              </a>
              <div class="dropdown-menu" aria-labelledby="library">
                  <a class="dropdown-item" href="library.php">Manage books</a>
@@ -72,27 +130,16 @@
                  <div class="dropdown-divider"></div>
                  <a class="dropdown-item" href="library_dashboard">Library Home</a>
              </div>
-        </div>
+         </div>
 
-             <a class="list-group-item list-group-item-action" href="Foodorder.php"> <i class="fas fa-hamburger"></i> Canteen</a>
+         <a class="list-group-item list-group-item-action" href="Foodorder.php"> <i class="fas fa-hamburger"></i>
+             Canteen</a>
 
-             <a class="list-group-item list-group-item-action" href="#"><i class="fab fa-facebook-messenger"></i> Messaging</a>
-        <div class="dropdown">
-             <a class="list-group-item list-group-item-action dropdown-toggle" href="#" id="library" role="button" data-toggle="dropdown"
-                 aria-haspopup="true" aria-expanded="false">
-                 <i class="far fa-grin"></i> Feedback 
-             </a>
-             <div class="dropdown-menu" aria-labelledby="library">
-                 <a class="dropdown-item" href="feedbacksummery.php">Feedback info</a>
-                 <a class="dropdown-item" href="feedback.php">Student report feedback</a>
-               
-                 <div class="dropdown-divider"></div>
-                 <a class="dropdown-item" href="Feedback_dashboard">Feedback Home</a>
-             </div>
-        </div>
-             <a class="list-group-item list-group-item-action" href="onpeak.php"><i class="fas fa-leaf"></i> Onpeak</a>
+         <a class="list-group-item list-group-item-action" href="#"><i class="fab fa-amazon-pay"></i> Payment </a>
+        
+         <a class="list-group-item list-group-item-action" href="onpeak.php"><i class="fas fa-leaf"></i> Onpeak</a>
 
-             <a class="list-group-item list-group-item-action" href="notice.php"><i class="fas fa-"></i>Notice</a>
+         <a class="list-group-item list-group-item-action" href="notice.php"><i class="fas fa-"></i>Notice</a>
 
      </div>
  </div>
@@ -100,19 +147,23 @@
  <!-- Page Content -->
  <div id="page-content-wrapper">
 
-     <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom">
          <button class="btn btn-dark" id="menu-toggle"><i class="fas fa-bars"></i> </button>
 
          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
              <span class="navbar-toggler-icon"></span>
          </button>
-         <div id="timestamp" class="pr-5 pl-2 ml-auto"></div>  
+         <div id="timestamp" class="pr-5 pl-2 ml-auto"></div>
          <div class="collapse navbar-collapse" id="navbarSupportedContent">
              <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
                  <li class="nav-item">
-                     <a class="nav-link" href="notifications.php"><i class="fas fa-bell"></i> <span
+                     <a class="nav-link" href="chat.php"><i class="fab fa-facebook-messenger"></i> <span
                              class="badge badge-primary badge-pill">14</span></a>
+                 </li>
+                 <li class="nav-item">
+                     <a class="nav-link" href="notifications.php"><i class="fas fa-bell"></i> <span
+                             class="badge badge-warning badge-pill">14</span></a>
                  </li>
                  <li class="nav-item dropdown">
                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
