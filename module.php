5320829f829c8886f86372c5de1f75a41a77e832
princeyprincey <princeythiru@gmail.com>
2019-09-09 12:14:28 +0530

    update module file

--- module.php
+++ module.php
@@ -10,7 +10,7 @@ include_once ("menu.php");
 
 
 <!-- Block#2 start your code -->
-<div class="container">
+
 <hr class="mb-8 mt-4">
   
 		<div class="card mt-12 ">
@@ -18,14 +18,12 @@ include_once ("menu.php");
 				<h4 align="center">ADD Module Details</h4><br>
       </div>
       </div>
-      </div>
-       
       <br>
       <br>
             <div class="row">
               <div class="col-md-6 mb-3">
-                <label for="firstName">Module ID</label>
-                <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+                <label for="ID">Module ID</label>
+                <input type="text" class="form-control" id="ID" placeholder="" value="" required>
                 <div class="invalid-feedback">
                   Valid Module ID is required.
                 </div>
@@ -33,28 +31,26 @@ include_once ("menu.php");
 
               
               <div class="col-md-6 mb-3">
-                <label for="firstName">Course Name</label>
-                <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+                <label for="Name">Module Name</label>
+                <input type="text" class="form-control" id="Name" placeholder="" value="" required>
                 <div class="invalid-feedback">
-                  Valid Course Name is required.
+                  Valid Module Name is required.
                 </div>
                 
                 <div class="invalid-feedback">
-                  Please provide a Course name
+                  Please provide a Module name
                 </div>
               </div>
-
-            
 
             </div>
             <div class="row">
             <div class="col-md-6 mb-3"> 
-              <label for="username">Duration-Institute Learning</label>
+              <label for="National">National Hours</label>
               <div class="input-group">
                 <div class="input-group-prepend">
-                  <span class="input-group-text">Months</span>
+                  <span class="input-group-text">Hrs</span>
                 </div>
-                <input type="text" class="form-control" id="username" placeholder="Month in Digits" required>
+                <input type="text" class="form-control" id="National" placeholder="Hours in Digits" required>
                 
                 <div class="invalid-feedback" style="width: 50%;">
                   Duration is required.
@@ -66,12 +62,12 @@ include_once ("menu.php");
             </div>
 
             <div class="col-md-6 mb-3"> 
-              <label for="username">Duration-OJT</label>
+              <label for="Learning">Learning Hours</label>
               <div class="input-group">
                 <div class="input-group-prepend">
-                  <span class="input-group-text">Months</span>
+                  <span class="input-group-text">Hrs</span>
                 </div>
-                <input type="text" class="form-control" id="username" placeholder="Month in Digits" required>
+                <input type="text" class="form-control" id="Learning" placeholder="Hours in Digits" required>
                 
                 <div class="invalid-feedback" style="width: 50%;">
                   Duration is required.
@@ -87,48 +83,85 @@ include_once ("menu.php");
 
             </div>
 
-           
-
             <div class="row">
-
               <div class="col-md-6 mb-3">
-              <label for="state">Department</label>
-                  <select class="custom-select d-block w-100" id="state" required>
-                    <option value="">Choose...</option>
-                    <option>ICT</option>
-                    <option>Construction</option>
-                    <option>Mechanical</option>
-                    <option>Electrical</option>
-                    <option>Automobile</option>
-                    <option>Food Dept</option>
-                  </select>
+                <label for="semister">Semister</label>
+                <input type="text" class="form-control" id="semister" placeholder="" value="" required>
                 <div class="invalid-feedback">
-                  Please provide a Department.
+                  Valid semister is required.
                 </div>
               </div>
 
+              
               <div class="col-md-6 mb-3">
-                <label for="country">NVQ Level</label>
-                  <select class="custom-select d-block w-100" id="country" required>
-                    <option value="">Choose...</option>
-                    <option>5</option>
-                    <option>4</option>
-                  </select>
-                  <div class="invalid-feedback">
-                    Please select a valid Level.
-                  </div>
+                <label for="unit">Relative Unit</label>
+                <input type="text" class="form-control" id="unit" placeholder="" value="" required>
+                <div class="invalid-feedback">
+                  Valid Relative Unit is required.
+                </div>
+                
+                <div class="invalid-feedback">
+                  Please provide a Relative Unit name
+                </div>
               </div>
-                <br><br>
 
-            </div>
-            
-            
+              </div>
+               
+                
+            <div class="row">
+              <div class="col-md-3 mb-3">
+                <label for="aim">Module aim</label>
+                <textarea class="form-control" id="Module aim" rows="3"></textarea>
+                <div class="invalid-feedback">
+                  Module is required.
+                </div>
+              </div>
+
+              
+              <div class="col-md-3 mb-3">
+                <label for="unit">Learning Outcomes</label>
+                <textarea class="form-control" id="Outcomes" rows="3"></textarea>
+                    <div class="invalid-feedback">
+                    Learning Outcomes is required.
+                    </div>
+                
+                    <div class="invalid-feedback">
+                    Please provide a Relative Unit name
+                    </div>
+              </div>
+
+              <div class="col-md-3 mb-3">
+                <label for="resources">Resources</label>
+                <textarea class="form-control" id="resources" rows="3"></textarea>
+                <div class="invalid-feedback">
+                Resources is required.
+                </div>
+              </div>
+
+              
+              <div class="col-md-3 mb-3">
+                <label for="references">References</label>
+                <textarea class="form-control" id="References" rows="3"></textarea>
+                    <div class="invalid-feedback">
+                    References is required.
+                    </div>
+                
+                    <div class="invalid-feedback">
+                    Please provide References name
+                    </div>
+              </div>
+
+              </div>
+
             <br>
-            <button class="btn btn-primary btn-lg btn-block" type="submit">Continue to Add Course Details</button>
+            <button class="btn btn-primary btn-lg btn-block" type="submit">Continue to Add Module Details</button>
           </form>
         </div>
       </div>
 </div>
+     
+       
+      
       <body>
 
 
