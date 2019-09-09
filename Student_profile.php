16ae68f682ea4599fe8c71d6d26c87e95871d0aa
thanujahthanujah <thanujah1996@gmail.com>
2019-09-09 12:10:05 +0530

    new

---
+++ Student_profile.php
@@ -0,0 +1,27 @@
+<!------START DON'T CHANGE ORDER HEAD,MANU,FOOTER----->
+<!---BLOCK 01--->
+<?php 
+   
+include_once("CONFIG.PHP");
+
+$title ="STUDENT PROFILE | SLGTI"; //YOUR HEAD TITLE CREATE VARIABLE BEFORE FILE NAME
+include_once("HEAD.PHP");
+include_once("MENU.PHP");
+
+?>
+<!----END DON'T CHANGE THE ORDER---->
+
+
+<!---BLOCK 02--->
+<!---START YOUR CODER HERE----->
+
+
+<!-----END YOUR CODE----->
+<!-- form start---->
+
+
+
+
+<!---BLOCK 03--->
+<!----DON'T CHANGE THE ORDER--->
+<?php include_once("FOOTER.PHP"); ?>
