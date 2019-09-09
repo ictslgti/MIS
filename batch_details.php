61ca2b27e4f203859e87b61fcf92d833e6783f76
abdullahart1991 <artabdullah31@gmail.com>
2019-09-09 12:00:14 +0530

    file update

---
+++ batch_details.php
@@ -0,0 +1,96 @@
+<!-- BLOCK#1 START DON'T CHANGE THE ORDER -->
+<?php 
+$title = "Department Details | SLGTI" ;
+include_once("config.php"); 
+include_once("head.php"); 
+include_once("menu.php");
+ ?>
+<!-- END DON'T CHANGE THE ORDER -->
+
+<!-- BLOCK#2 START YOUR CODER HERE -->
+<h1>Batch Details of ICT Department</h1>
+<button type="button" class="btn btn-success">+ New Batch</button>
+
+<table class="table table-hover">
+  <thead class="thead-dark">
+ 
+    <tr >
+    <th scope="col">Batch_ID</th>
+      <th scope="col">Course Name</th>
+      <th scope="col">Academic Year</th>
+      <th scope="col">Options</th>
+
+     
+      
+    </tr>
+  </thead>
+  <tbody>
+ 
+  <tr class="table-light">
+      
+     
+      <td>5IT01</td>
+      <td><div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">NVQ Level - 05
+  <div class="btn-group mr-2" role="group" aria-label="First group">
+  <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button"  class="btn-group btn-group-sm">View</button></td>  
+      <td>2018/2019 <span class="badge badge-success">Active </span> </td> 
+      <td><div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+  <div class="btn-group mr-2" role="group" aria-label="First group">
+  <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn-group btn-group-sm">Students</button>
+    </tr>
+    
+   
+    <tr class="table-light">
+      <td>5IT01</td>
+      <td><div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">NVQ Level - 04
+  <div class="btn-group mr-2" role="group" aria-label="First group">
+  <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn-group btn-group-sm">View</button></td>  
+      <td>2018/2019 <span class="badge badge-success">Active </span></td> 
+      <td><div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+  <div class="btn-group mr-2" role="group" aria-label="First group">
+  <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn-group btn-group-sm">Students</button>
+    </tr>
+   
+
+    <tr class="table-light">
+      <td>5IT02</td>
+      <td><div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">NVQ Level - 05
+  <div class="btn-group mr-2" role="group" aria-label="First group">
+  <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn-group btn-group-sm">View</button></td>  
+      <td>2019/2020 <span class="badge badge-success">New </span></td> 
+      <td><div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+  <div class="btn-group mr-2" role="group" aria-label="First group">
+  <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn-group btn-group-sm">Students</button>
+    </tr>
+
+    <tr class="table-light">
+      <td>5IT02</td>
+      <td><div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">NVQ Level - 04
+  <div class="btn-group mr-2" role="group" aria-label="First group">
+  <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn-group btn-group-sm">View</button></td>  
+      <td>2019/2020 <span class="badge badge-success">New </span></td> 
+      <td><div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+  <div class="btn-group mr-2" role="group" aria-label="First group">
+  <div class="input-group-text" ><i class="fas fa-eye"></i></div>
+    <button type="button" class="btn-group btn-group-sm">Students</button>
+    </tr>
+
+
+  </tbody>
+  </form>
+</table>
+
+<!-- END YOUR CODER HERE -->
+
+    <!-- BLOCK#3 START DON'T CHANGE THE ORDER -->
+    <?php 
+    include_once("footer.php");
+    ?>
+    <!-- END DON'T CHANGE THE ORDER -->
