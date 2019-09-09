9c59a6a5c87d9c027bdf5c83985d88c2145a2fd4
thanujahthanujah <thanujah1996@gmail.com>
2019-09-09 15:59:28 +0530

    form

--- entroll_st_course.php
+++ entroll_st_course.php
@@ -18,7 +18,50 @@ include_once("MENU.PHP");
 
 <!-----END YOUR CODE----->
 <!-- form start---->
-<h1> STUDENT ENTROLL COURSE DETAILS  </h1>
+
+    <div class="ROW">
+        <div class="col text-center">
+            <h2>STUDENT AND COURSE DETAILS</h2>   
+        </div>
+    </div>
+
+    <form class="needs-validation" novalidate action="">
+    <div class="form-row">
+          <div class class="col-md-1">
+          </div>
+          <div class="col">
+          <p style="font-size: 20px; font-weight: 700; border-bottom: 2px solid #aaa;">ENTROLLMENT</p>
+          </div>  
+    </div> 
+                      
+    <div class="form-row">
+          <div class="col-md-12 mb-3">
+          Student_Id :
+          <input type="text"  id="id" placeholder="" aria-describedby="idPrepend" required>
+         </div>
+    </div>
+
+    <div class="form-row">
+          <div class="col-md-12 mb-3">
+          Course_Id :
+          <input type="text"  id="no" placeholder="" aria-describedby="noPrepend" required>
+         </div>
+    </div>
+
+    <div class="form-row">
+          <div class="col-md-12 mb-3">
+          Entroll Year :
+          <input type="text"  id="year" placeholder="" aria-describedby="fullnamePrepend" required>
+         </div>
+    </div>
+
+    
+    <div class="form-row">
+        <button type="button" class="btn btn-primary">ADD</button><br>
+        <button type="button" class="btn btn-success">UPDATE</button><br>
+        <button type="button" class="btn btn-danger">DELETE</button><br>
+    </div>
+</form>
 
 
 
