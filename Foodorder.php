d45763f28c0a4d559d7288e9153e287c11148881
GAJANGTR9060 <sivagajan96in@gmail.com>
2019-09-09 10:37:02 +0530

    add menu lists

--- Foodorder.php
+++ Foodorder.php
@@ -12,6 +12,109 @@ $title = "Home | SLGTI";
 
 <div class="container-fluid">
      <!-- FOOD MENU DESIGN    -->
+     <div class="row">
+    <div class="col-12">
+        <h1 class="h1">Food Menu</h1>
+            <p> Foods very Testy and best quality.   </p>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row" style="margin-top:3%;">
+        <div class="col container">
+            <div class="card" style="width:80%;Height:60% ;margin-top:3%;">
+                <img class="card-img-top" src="img/123.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Monday</h5>
+                    <a href="#" class="btn btn-primary">Click Here</a>
+                </div>
+            </div>
+        </div>
+        <div class="col container">
+            <div class="card" style="width:80%;Height:60%;margin-top:3%;">
+                <img class="card-img-top" src="img/154.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Tuesday</h5>
+                    <a href="#" class="btn btn-primary">Click Here</a>
+                </div>
+            </div>
+        </div>
+        <div class="col container">
+            <div class="card" style="width:80%;Height:60%;margin-top:3%;">
+                <img class="card-img-top" src="img/564.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Wednesday</h5>
+                    <a href="#" class="btn btn-primary">Click Here</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row" style="margin-top:3%;">
+        <div class="col container">
+            <div class="card" style="width:80%;Height:60%; margin-top:3%;">
+                <img class="card-img-top" src="img/234.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Thursday</h5>
+                    <a href="#" class="btn btn-primary">Click Here</a>
+                </div>
+            </div>
+        </div>
+        <div class="col container">
+            <div class="card" style="width:80%;Height:60%; margin-top:3%;">
+                <img class="card-img-top" src="img/789.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Friday</h5>
+                    <a href="#" class="btn btn-primary">Click Here</a>
+                </div>
+            </div>
+        </div>
+        <div class="col container">
+            <div class="card" style="width:80%;Height:60%;margin-top:3%;">
+                <img class="card-img-top" src="img/987.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Saturday</h5>
+                    <a href="#" class="btn btn-primary">Click Here</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row" style="margin-top:3%;">
+        <div class="col container">
+            <div class="card" style="width:80%;Height:60% ;margin-top:3%;">
+                <img class="card-img-top" src="img/369.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Sunday</h5>
+                    <a href="#" class="btn btn-primary">Click Here</a>
+                </div>
+            </div>
+        </div>
+        <div class="col container invisible">
+            <div class="card" style="width:50%;Height:50% ;margin-top:3%;">
+                <img class="card-img-top" src="img/New spl.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Tuesday</h5>
+                    <a href="#" class="btn btn-primary">Click Here</a>
+                </div>
+            </div>
+        </div>
+        <div class="col container invisible">
+            <div class="card" style="width:50%;Height:50%;margin-top:3%;">
+                <img class="card-img-top" src="img/Thu spl.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Wednesday</h5>
+                    <a href="#" class="btn btn-primary">Click Here</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 
     <!-- ORDE CART DESIGN  -->
