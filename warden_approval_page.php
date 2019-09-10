5dab67fd7b0f90610f089d25cfa5c87f01b01a0f
sangeevan1 <sangeevan13@gmail.com>
2019-09-10 11:49:34 +0530

    sangee

---
+++ warden_approval_page.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" ">
+
+    <title>Warden dashboard</title>
+  </head>
+  <body>
+  <div class="card mb-md-5">
+  <h1 class="h4 text-center p-3 mb-2 bg-primary text-white">Off-peak Requests</h1>
+
+    <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name of applicant</th>
+      <th scope="col">Registration No</th>
+      <th scope="col">Department</th>
+      <th scope="col">Contact No</th>
+      <th scope="col">Date</th>
+      <th scope="col">Time</th>
+      <th scope="col">Reason for exit</th>
+      <th colspan="3">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>????</td>
+      <td>????</td>
+      <td>????</td>
+      <td>?????</td>
+      <td>?????</td>
+      <td>????</td>
+      <td>??????</td>
+      <td><button type="button" class="btn btn-success btn-sm">Approve</button>   <button type="button" class="btn btn-danger btn-sm">Reject</button></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>????</td>
+      <td>????</td>
+      <td>????</td>
+      <td>?????</td>
+      <td>?????</td>
+      <td>????</td>
+      <td>??????</td>
+      <td><button type="button" class="btn btn-success btn-sm">Approve</button>   <button type="button" class="btn btn-danger btn-sm">Reject</button></td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>????</td>
+      <td>????</td>
+      <td>????</td>
+      <td>?????</td>
+      <td>?????</td>
+      <td>????</td>
+      <td>??????</td>
+      <td><button type="button" class="btn btn-success btn-sm">Approve</button>   <button type="button" class="btn btn-danger btn-sm">Reject</button></td>
+    </tr>
+    
+  </tbody>
+</table>
+
+ 
+  </tbody>
+</table>
+</div>
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
