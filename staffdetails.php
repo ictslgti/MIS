0c485afba6f690fd6e40e0a3b08ec29c6bb64614
ashviniselvaratnam <ashviniselvaratnam@gmail.com>
2019-09-10 09:57:43 +0530

    staffdetails“

--- staffdetails.php
+++ staffdetails.php
@@ -11,14 +11,7 @@ include_once("menu.php");
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>staff</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/styles.min.css">
-    <style>
-      
-    </style>
+  
 </head>
 <body>
 <h1 class="text-center">STAFF DETAILS</h1>
