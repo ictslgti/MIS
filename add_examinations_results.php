f51b603deabcdf3dff6f3bb6a448cf3b93373a8c
midhushan <mithunanirudh@gmail.com>
2019-09-10 15:09:23 +0530

    add new features

--- add_examinations_results.php
+++ add_examinations_results.php
@@ -1,5 +1,5 @@
 <?php
-$title = "Department Details | SLGTI";
+$title = "Examinations | SLGTI";
  include_once("config.php"); 
  include_once("head.php"); 
  include_once("menu.php"); 
@@ -8,67 +8,47 @@ $title = "Department Details | SLGTI";
 
 <!--BLOCK#2 START MY CODER HERE -->
 <html>
-
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-        <title>examinations</title>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/styles.min.css">
-        <!-- my css -->
-        <style>
-        th, td {
-            text-align: center;
-        }
-
-
-        </style>
-        <!-- my css end -->
-
-    </head>
-    <!-- header text -->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/script.min.js"></script>
-
     <body style="background-color: rgb(255,255,255);">
 
-        <!-- Main PHP Declarations  -->
         <?php
     $ictDepartmentName = "Department Of Information & Communication Technology";
     ?>
 
-    <div class="highlight-blue">
-        <div class="container">
-            <div class="intro">
-                <h1 class="display-4 text-center">Add Asignments Results</h1>
-                <H3 class="display-5 text-center"><?php echo $ictDepartmentName ?></H3>
-                <p class="text-center">Welcome to examinations portal for lectures or admin. This section to add
-                    examinations and assignments/asessments results&nbsp;</p>
 
+
+        <div class="highlight-blue">
+            <div class="container">
+                <div class="intro">
+                    <h1 class="display-4 text-center">Asignments Portal</h1>
+                    <H3 class="display-5 text-center"><?php echo $ictDepartmentName ?></H3>
+                    <p class="text-center">Welcome to examinations portal for lectures or admin. This section to add
+                        examinations and assignments/asessments results&nbsp;</p>
+
+                </div>
             </div>
         </div>
-    </div>
-
-<!-- <div>
-    <img src="img/add_examinations_results_main_cart.jpg" class="img-fluid" alt="Responsive image">
-</div> -->
-    
-    <br>
-
-        <!-- <div class="card mb-3">
-            
-            <img src="img/add_examinations_results_main_cart.jpg" class="card-img-top" alt="exampic">
-            <div class="card-body">
+        <!-- card start -->
+        <div class="card">
+            <br>
+            <div class="container">
+                <div class="intro">
+                    <h3 class="display-5 text-center">Add Asignments Marks</h3>
+                </div>
             </div>
-        </div> -->
+            <br>
+        </div>
+        <!-- end start -->
 
-        
+        <br>
+
+
 
         <!-- mainform start  -->
         <div class="input-group mb-3">
             <div class="input-group-prepend">
-                <label class="input-group-text" for="inputGroupSelect01">Select Semister</label>
+                <label class="input-group-text" for="inputGroupSelect01"><i
+                        class="fas fa-graduation-cap"></i>&nbsp;&nbsp;Select
+                    Semister&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
             </div>
             <select class="custom-select" id="inputGroupSelect01">
                 <option selected>Choose...</option>
@@ -79,7 +59,9 @@ $title = "Department Details | SLGTI";
 
         <div class="input-group mb-3">
             <div class="input-group-prepend">
-                <label class="input-group-text" for="inputGroupSelect01">Select Module</label>
+                <label class="input-group-text" for="inputGroupSelect01"><i
+                        class="fas fa-book-open"></i>&nbsp;&nbsp;Select
+                    Module&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
             </div>
             <select class="custom-select" id="inputGroupSelect01">
                 <option selected>Graphic Design</option>
@@ -93,7 +75,8 @@ $title = "Department Details | SLGTI";
 
         <div class="input-group mb-3">
             <div class="input-group-prepend">
-                <label class="input-group-text" for="inputGroupSelect01">Select Asignments</label>
+                <label class="input-group-text" for="inputGroupSelect01"><i
+                        class="fas fa-chalkboard-teacher"></i>&nbsp;&nbsp;Select Asignments</label>
             </div>
             <select class="custom-select" id="inputGroupSelect01">
                 <option selected>Choose...</option>
@@ -105,13 +88,18 @@ $title = "Department Details | SLGTI";
 
         <div class="container">
             <div class="row justify-content-md-center">
-              
-              <div class="col-md-auto">
-                <!-- main button  -->
-                <button type="button" class="btn btn-outline-primary"><i class="fas fa-plus"></i>&nbsp;Add Results</button>
-              </div>
-             
+                <div class="col-md-auto">
+                    <!-- main button  -->
+                    <button type="button" class="btn btn-outline-primary"><i class="fas fa-plus"></i>&nbsp;Add
+                        Results</button>
+                </div>
             </div>
+        </div>
+        <!-- main form end -->
+
+        <br>
+        <br>
+        <br>
 
         <?php
         // modules for ict sem1
@@ -125,39 +113,16 @@ $title = "Department Details | SLGTI";
 
 
      ?>
-
-        <h1 class="display-4">
-            <center><?php echo $Sem1module1 ?></center>
-        </h1>
-
-
-
-        <!-- <form>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="inputPassword4">Student Roll Number </label>
-                    <input type="email" class="form-control" id="inputEmail4" placeholder=""><br>
-
-                    <label for="inputPassword4">Student Full Name </label>
-                    <input type="email" class="form-control" id="inputEmail4" placeholder="">
-
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="inputPassword4">Module2</label>
-                    <input type="password" class="form-control" id="inputPassword4" placeholder="">
+        <div class="card">
+            <br>
+            <div class="container">
+                <div class="intro">
+                    <h3 class="display-5 text-center">Enter Asignments Marks</h3>
                 </div>
             </div>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="inputEmail4">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4" placeholder="">
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="inputPassword4">Password</label>
-                    <input type="password" class="form-control" id="inputPassword4" placeholder="">
-                </div>
-            </div>
-        </form><br> -->
+            <br>
+        </div>
+        <br>
 
         <!-- table -->
         <table class="table">
@@ -165,15 +130,27 @@ $title = "Department Details | SLGTI";
                 <tr>
                     <th scope="col">Student Roll Number</th>
                     <th scope="col">Student Full Name</th>
-                    <th scope="col">Module</th>
                     <th scope="col">Marks</th>
+                    <th scope="col">Grade</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <th scope="row">1</th>
                     <td>Mark</td>
-                    <td>Otto</td>
+                    <td>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Enter the Marks" aria-label="Username"
+                                aria-describedby="basic-addon1">
+                        </div>
+                    </td>
+                    <td scope="row"><span class="badge badge-success">&nbsp;&nbsp;&nbsp;&nbsp;Pass&nbsp;&nbsp;&nbsp;&nbsp;</span></td>
+                </tr>
+                <tr>
+                    
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+
                     <td>
                         <div class="input-group mb-3">
 
@@ -183,6 +160,82 @@ $title = "Department Details | SLGTI";
                     </td>
                 </tr>
                 <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+
+                    <td>
+                        <div class="input-group mb-3">
+
+                            <input type="text" class="form-control" placeholder="Enter the Marks" aria-label="Username"
+                                aria-describedby="basic-addon1">
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <!-- end of add table -->
+        <br>
+        <!-- save button start -->
+        <div class="row">
+            <div class="col">
+
+            </div>
+            <div class="col-md-auto">
+
+            </div>
+            <div class="col col-lg-2">
+                <button type="button" class="btn btn-outline-primary">&nbsp;&nbsp;&nbsp;<i
+                        class="fas fa-save"></i>&nbsp;&nbsp;Save&nbsp;&nbsp;&nbsp;</button>
+            </div>
+        </div>
+        <!-- save button end -->
+        <br>
+        <br>
+<!-- card start -->
+        <div class="card">
+            <br>
+            <div class="container">
+                <div class="intro">
+                    <h3 class="display-5 text-center">Overall Module Marks</h3>
+                </div>
+            </div>
+            <br>
+        </div>
+        <!-- end card -->
+        <br>
+        <div class="row">
+            <div class="col">
+                1 of 2
+            </div>
+            <div class="col">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Student's Index Number"
+                        aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon2">&nbsp;&nbsp;&nbsp;<i
+                                class="fas fa-search"></i>&nbsp;&nbsp;&nbsp;Search&nbsp;&nbsp;&nbsp;</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- small view table start  -->
+        <table class="table table-sm">
+            <thead>
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Student Index Number</th>
+                    <th scope="col">Full Name</th>
+                    <th scope="col">Module Marks</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
                     <th scope="row">2</th>
                     <td>Jacob</td>
                     <td>Thornton</td>
@@ -190,30 +243,35 @@ $title = "Department Details | SLGTI";
                 </tr>
                 <tr>
                     <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
+                    <td colspan="2">Larry the Bird</td>
                     <td>@twitter</td>
                 </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                </tr>\
             </tbody>
         </table>
+        <!-- small view table end -->
+        <!-- print mode start -->
+        <div class="row">
+            <div class="col">
+
+            </div>
+            <div class="col-md-auto">
+
+            </div>
+            <div class="col col-lg-2">
+                <button type="button" class="btn btn-outline-primary">&nbsp;&nbsp;&nbsp;<i class="fas fa-print"></i>&nbsp;&nbsp;Print&nbsp;&nbsp;&nbsp;</button>
+            </div>
+        </div>
+
+
+
+        <!-- end mode  -->
+
+
+
 
     </body>
 
 </html>
-
-
 <!-- END OF MY CODE -->
 
 
