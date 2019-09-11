e94eac4f9df7c79f0567ea665821ed7f11c6c999
GAJANGTR9060 <sivagajan96in@gmail.com>
2019-09-11 09:28:36 +0530

    name changed

---
+++ FoodOrders.php
@@ -0,0 +1,245 @@
+<!-- BLOCK#1 START DON'T CHANGE THE ORDER-->  
+<?php
+$title = "Home | SLGTI";
+ include_once("config.php"); 
+ include_once("head.php"); 
+ include_once("menu.php"); 
+ ?>
+<!--END DON'T CHANGE THE ORDER--> 
+
+<!--BLOCK#2 START YOUR CODE HERE -->
+<div class="row">
+    <div class="col-sm-12 col-md-4 col-lg-8 container">
+        <h1 class="display-3"><em>EAT GOOD FEEL GOOD</em></h1>
+    </div>
+
+    <div class="col-sm-12 col-md-4 col-lg-2 container">     
+        <div class="dropdown">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">WEEK DAYS</a>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="#">MONDAY</a>
+                <a class="dropdown-item" href="#">TUESDAY</a>
+                <a class="dropdown-item" href="#">WEDNESDAY</a>
+                <a class="dropdown-item" href="#">THURSDAY</a>
+                <a class="dropdown-item" href="#">FRIDAY</a>
+                <a class="dropdown-item" href="#">SATURDAY</a>
+                <a class="dropdown-item" href="#">SUNDAY</a>
+            </div>
+         </div>
+    </div>
+</div>
+
+ <!-- FOOD MENU DESIGN    -->
+ <!-- MORNING FOODS     -->
+ <h4 class="display-"><em>Morning Foods</em></h4>
+        <div class="row">
+            <div class="col-sm-12 col-md-4 col-lg-3 container">
+                <div class="card" style="width:80%;Height:60% ;margin-top:3%;">
+                    <img class="card-img-top" src="img/154.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Rotti  1 Pc<br>
+                         20/=</h5>
+                        <a href="#" class="btn btn-primary">Add</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-3 container">
+                <div class="card" style="width:80%;Height:60%;margin-top:3%;">
+                    <img class="card-img-top" src="img/154.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Dosa  1 Pc<br>
+                        20/=</h5>
+                        <a href="#" class="btn btn-primary">Add</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-3 container">
+                <div class="card" style="width:80%;Height:60%;margin-top:3%;">
+                    <img class="card-img-top" src="img/154.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Idiyapam  10 Pc<br>
+                        40/=</h5>
+                        <a href="#" class="btn btn-primary">Add</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-3 container">
+                <div class="card" style="width:80%;Height:60%; margin-top:3%;">
+                    <img class="card-img-top" src="img/154.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Noodles
+                        1 Pk<br>
+                        80/=</h5>
+                        <a href="#" class="btn btn-primary">Add</a>
+                    </div>
+                </div>
+            </div>
+
+
+     <!-- LUNCH FOODS    -->
+
+    <h4 class="display-"><em>Lunch Foods</em></h4>
+            <div class="container-fluid"> 
+        <div class="row">
+        <div class="col-sm-12 col-md-4 col-lg-3 container">
+                <div class="card" style="width:80%;Height:60% ;margin-top:3%;">
+                    <img class="card-img-top" src="img/154.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Egg Rice  1 Pk<br>
+                         80/=</h5>
+                        <a href="#" class="btn btn-primary">Add</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-3 container">
+                <div class="card" style="width:80%;Height:60%;margin-top:3%;">
+                    <img class="card-img-top" src="img/154.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Veg Rice  1 Pk<br>
+                        60/=</h5>
+                        <a href="#" class="btn btn-primary">Add</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-3 container">
+                <div class="card" style="width:80%;Height:60%;margin-top:3%;">
+                    <img class="card-img-top" src="img/154.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Fish Rice  1Pk<br>
+                        80/=</h5>
+                        <a href="#" class="btn btn-primary">Add</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-3 container">
+                <div class="card" style="width:80%;Height:60%; margin-top:3%;">
+                    <img class="card-img-top" src="img/154.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">TODAY SPL</h4>
+                        <a href="#" class="btn btn-primary">Add</a>
+                    </div>
+                </div>
+            </div>
+ 
+
+
+
+
+ <!-- DINNER FOODS    -->
+
+ <h4 class="display-"><em>Dinner Foods</em></h4>
+            <div class="container-fluid"> 
+        <div class="row">
+        <div class="col-sm-12 col-md-4 col-lg-3 container">
+                <div class="card" style="width:80%;Height:60% ;margin-top:3%;">
+                    <img class="card-img-top" src="img/154.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Kotthu  1 Pk<br>
+                         100/=</h5>
+                        <a href="#" class="btn btn-primary">Add</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-3 container">
+                <div class="card" style="width:80%;Height:60%;margin-top:3%;">
+                    <img class="card-img-top" src="img/154.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Fired Rice  1 Pk<br>
+                        100/=</h5>
+                        <a href="#" class="btn btn-primary">Add</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-3 container">
+                <div class="card" style="width:80%;Height:60%;margin-top:3%;">
+                    <img class="card-img-top" src="img/154.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Pittu  1 Pk<br>
+                        80/=</h5>
+                        <a href="#" class="btn btn-primary">Add</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col container invisible">
+                <div class="card" style="width:80%;Height:60%; margin-top:3%;">
+                    <img class="card-img-top" src="img/154.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">TODAY SPL</h4>
+                        <a href="#" class="btn btn-primary">Add</a>
+                    </div>
+                </div>
+            </div>
+ 
+
+        <!-- ORDE CART DESIGN  -->
+  
+    <div class="row ">
+        <div class="col-sm-12 col-md-4 col-lg-8 container"><h4 class="font-italic">     
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Item ID</th>
+                        <th scope="col">Item Name</th>
+                        <th scope="col">Qty</th>
+                        <th scope="col">Amount</th>
+                    </tr>
+                 </thead>
+                <tbody class="table-borderless">
+                    <tr>
+                        <td>1</td>
+                        <td>Pittu</td>
+                        <td></td>
+                        <td>60</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>idiyapam</td>
+                        <td></td>
+                        <td>100</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="col-sm-12 col-md-4 col-lg-1 container">
+        </div>
+
+        <div class="col-sm-12 col-md-4 col-lg-3 container">
+        <div class="card bg-light mb-3" style="max-width: 18rem;">
+            <div class="card-header">Header</div>
+                <div class="card-body">
+
+                    <div class="row">
+                        <div class="col">
+                            <p>Order ID</p>
+                        </div>
+                        <div class="col">
+                            <p>1</p>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col">
+                            <p>Total</p>
+                        </div>
+                        <div class="col">
+                            <p>1000</p>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <button type="button" class="btn btn-success w-75">Order</button>
+                    </div>
+                    
+                </div>
+            </div>      
+        </div>
+    </div>
+
+
+   
+<!--END OF YOUR COD-->
+
+<!--BLOCK#3 START DON'T CHANGE THE ORDER-->   
+<?php include_once("footer.php"); ?>
+<!--END DON'T CHANGE THE ORDER-->  
