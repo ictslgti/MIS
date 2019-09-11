120ffd4de5fd392d09d22c34d3246f2b65ba9f53
midhushan <mithunanirudh@gmail.com>
2019-09-11 10:13:38 +0530

    new

--- Assessment.php
+++ Assessment.php
@@ -6,6 +6,47 @@ $title = "Examinations | SLGTI";
  ?>
 <!--END DON'T CHANGE THE ORDER-->
 
+<div class="shadow p-3 mb-5 bg-white rounded">
+
+        <div class="highlight-blue">
+            <div class="container">
+                <div class="intro">
+                    <h1 class="display-4 text-center">Asignments Portal</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <table class="table shadow p-3 mb-5 bg-white rounded">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
 
 
 
