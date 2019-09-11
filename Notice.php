534483efb3670f94749ef60f0cadfe30fea806c4
pirujeyam1995 <pirujeyam1995@gmail.com>
2019-09-11 10:33:25 +0530

    Notice info“

---
+++ Notice.php
@@ -0,0 +1,100 @@
+<!-- BLOCK#1 START DON'T CHANGE THE ORDER-->
+<?php
+$title = "Home | SLGTI";
+include_once("config.php");
+include_once("head.php");
+include_once("menu.php");
+?>
+<!--END DON'T CHANGE THE ORDER-->
+
+    <div class="row pl-5 pt-5">
+        <div class="col-md-4 col-sm-12 pl-5 ">
+            <div class="card" style="max-width: 20rem; margin-top:2%;">
+                <div class="card-body">
+                    <h5 class="card-title">Exam</h5>
+                    <p class="card-text">use submit a form to register your interest at least two months before the exam date.You will be sent an exam entry form and candidate instructions by the end of Month. 
+                                         You will also be sent an exam entry form if you are re-sitting the exam.</p>
+                         <a  href="notice_history.php">
+                          <button type="button" class="btn btn-primary">view</button>
+                    </div></a>
+              </div>
+            </div>
+        <div class="col-md-4 col-sm-12 pl-5 ">
+            <div class="card" style="max-width: 20rem; margin-top:2%;">
+                <div class="card-body">
+                    <h5 class="card-title">Result </h5>
+                    <p class="card-text">SLGTI is known for providing practice-oriented vocational training programmes at NVQ Level 4, 5 and 6, which contribute to a unique learning experience.
+                     Promoting a demand-oriented, , SLGTI is supporting the reconciliation process in Sri Lanka.</p>
+                     <a  href="notice_history.php">
+                    <button type="button" class="btn btn-primary">View</button>
+                </div></a>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-12 pl-5">
+            <div class="card" style="max-width: 20rem; margin-top:2%;">
+                <div class="card-body">
+                    <h5 class="card-title">Events</h5>
+                   
+                    <p class="card-text">Event planning, design and production within time limits. Working with clients to identify their needs and 
+                    Organizing facilities and details such as decor, catering, entertainment, transportation, location, invitee list, special guests, equipment, promotional material etc.</p>
+                    <a  href="notice_history.php">
+                    <button type="button" class="btn btn-primary">View</button>
+                </div></a>
+            </div>
+        </div>
+        <div class="w-100"></div>
+
+            <div class="col-md-4 col-sm-12 pl-5 ">
+                <a  href="notice_description.php" style="text-decoration:none">
+                    <div class="card text-white bg-primary mb-3 mt-5" style="max-width: 18rem;">
+                        <div class="card-header">
+                            <div class="row">
+                            <div class="col-6"> <h2>EXAM </h2> </div>
+                            <div class="col-3"></div>
+                            <div class="col-3"> <h2> <i class="fas fa-edit"></i>  </h2> </div>
+                            </div>
+                        </div>
+                            <div class="card-body">
+                                <h5 class="card-title">ADD THE EXAM </h5>
+                            
+                            </div>
+                    </div></a>
+            </div>
+        <div class="col-md-4 col-sm-12 pl-5 ">
+        <a  href="notice_description.php" style="text-decoration:none">
+            <div class="card text-white bg-secondary mb-3 mt-5" style="max-width: 18rem;">
+                 <div class="card-header">
+                 <div class="row">
+                    <div class="col-6"><H3> RESULT</H3></div>
+                    <div class="col-3"></div>
+                    <div class="col-3"> <h2> <i class="fas fa-user-graduate"></i> </h2> </div>
+                            </div>
+                        </div>
+                 <div class="card-body">
+                     <h5 class="card-title">ADD THE RESULT</h5>
+                     
+                </div>
+            </div></a>
+        </div>
+        <div class="col-md-4 col-sm-12 pl-5 ">
+            <div class="card text-white bg-success mb-3 mt-5" style="max-width: 18rem;">
+                 <div class="card-header">EVENTS</div>
+                
+                         <div class="list-group">
+                                <a href="notice_event_description" class="list-group-item list-group-item-action list-group-item-primary">Awading Ceremoney</a>
+                                 <a href="notice_event_description" class="list-group-item list-group-item-action list-group-item-danger">Blood Donation</a>
+                                <a href="notice_event_description" class="list-group-item list-group-item-action list-group-item-secondary">Christmas Day</a>
+                                <a href="notice_event_description" class="list-group-item list-group-item-action list-group-item-success">Hons Minister Visit</a>
+                                <a href="notice_event_description" class="list-group-item list-group-item-action list-group-item-">Others</a>
+                                
+                         </div>
+              </div>
+              </div>
+    </div>
+<!--BLOCK#2 START YOUR CODE HERE -->
+
+<!--END OF YOUR COD-->
+
+<!--BLOCK#3 START DON'T CHANGE THE ORDER-->
+<?php include_once("footer.php"); ?>
+<!--END DON'T CHANGE THE ORDER-->
