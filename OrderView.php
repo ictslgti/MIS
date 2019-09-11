c6b840723dd2c9340df2c7b69f45915fed910304
faheeeeeem <abdulwalith96@gmail.com>
2019-09-11 11:46:50 +0530

    new file

---
+++ OrderView.php
@@ -0,0 +1,94 @@
+ 
+<!--block 1 start dont change the order-->
+
+<?php 
+$title="View order | SLGTI";
+include_once("config.php");
+include_once("head.php");
+include_once("menu.php");
+?>
+  
+    <!-- end dont change the order-->
+<!--block 2 start my code here-->  
+
+   <div class= "row"> 
+   <div class ="col-3"><h1>"FOOD SHOULD BE FUN"</h1></div>
+   <div class ="col-4"><input type="date" class="form-control" id="datePic" aria-describedby="datePicHelp"></div>
+
+<div class ="col-3"><input type="search" class="form-control ds-input" id="search-input" placeholder="Search by Id..." aria-label="Search for..." 
+autocomplete="off" data-docs-version="4.3" spellcheck="false" role="combobox" aria-autocomplete="list" aria-expanded="false" 
+aria-owns="algolia-autocomplete-listbox-0" dir="auto" style="position: relative; vertical-align: top;">
+
+
+
+
+
+
+</div>
+  <div class ="col-1"><button type="button" class="btn btn-success">Go</button> </div>
+</div></div>
+<table class="table table-borderless ">
+  <thead class=" thead-dark">
+    <tr>
+      <th scope="col" >User name</th>
+      <th scope="col">User ID</th>
+      <th scope="col">Item Name</th>
+      <th scope="col">Qty</th>
+      <th scope="col">Total</th>
+
+     
+     
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Abdullah</th>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">Sahan</th>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">Ikas</th>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    
+    
+  </tbody>
+</table>
+
+
+
+   
+
+
+   </div>
+    
+  
+
+    
+
+
+    
+
+    
+  
+<!--block 3 start dont change the order-->
+
+    <?php include_once("footer.php"); ?>
+
+    
+    <!-- end dont change the order-->
