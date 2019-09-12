e6fe0c93a6eccdfc5a1416333a2cf23aadd4f0b4
thanujahthanujah <thanujah1996@gmail.com>
2019-09-12 12:54:34 +0530

    table

--- AddStudent.php
+++ AddStudent.php
@@ -182,74 +182,13 @@ include_once("menu.php");
     </div>
 
     <div class="row">
-<div class="col-md-6">
-<div class="col-md-8"><span class="wpcf7-form-control-wrap olsub1"><input type="text" name="olsub1" value="Mathematics" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div>
-<div class="col-md-4"><span class="wpcf7-form-control-wrap olgrd1"><input type="text" name="olgrd1" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div></div>
-<div class="col-md-6">
-<div class="col-md-8"><span class="wpcf7-form-control-wrap olsub2"><input type="text" name="olsub2" value="Language and Literature" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div>
-<div class="col-md-4"><span class="wpcf7-form-control-wrap olgrd2"><input type="text" name="olgrd2" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div></div>
-<div class="col-md-6">
-<div class="col-md-8"><span class="wpcf7-form-control-wrap olsub3"><input type="text" name="olsub3" value="English" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div>
-<div class="col-md-4"><span class="wpcf7-form-control-wrap olgrd3"><input type="text" name="olgrd3" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div></div>
-<div class="col-md-6">
-<div class="col-md-8"><span class="wpcf7-form-control-wrap olsub4"><input type="text" name="olsub4" value="Religion" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div>
-<div class="col-md-4"><span class="wpcf7-form-control-wrap olgrd4"><input type="text" name="olgrd4" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div></div>
-<div class="col-md-6">
-<div class="col-md-8"><span class="wpcf7-form-control-wrap olsub5"><input type="text" name="olsub5" value="History" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div>
-<div class="col-md-4"><span class="wpcf7-form-control-wrap olgrd5"><input type="text" name="olgrd5" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div></div>
-<div class="col-md-6">
-<div class="col-md-8"><span class="wpcf7-form-control-wrap olsub6"><input type="text" name="olsub6" value="Science" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div>
-<div class="col-md-4"><span class="wpcf7-form-control-wrap olgrd6"><input type="text" name="olgrd6" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div></div>
-<div class="col-md-6">
-<div class="col-md-8"><span class="wpcf7-form-control-wrap olsub7"><input type="text" name="olsub7" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="7" /></span></div>
-<div class="col-md-4"><span class="wpcf7-form-control-wrap olgrd7"><input type="text" name="olgrd7" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div></div>
-<div class="col-md-6">
-<div class="col-md-8"><span class="wpcf7-form-control-wrap olsub8"><input type="text" name="olsub8" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="8" /></span></div>
-<div class="col-md-4"><span class="wpcf7-form-control-wrap olgrd8"><input type="text" name="olgrd8" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div></div>
-<div class="col-md-6">
-<div class="col-md-8"><span class="wpcf7-form-control-wrap olsub9"><input type="text" name="olsub9" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="9" /></span></div>
-<div class="col-md-4"><span class="wpcf7-form-control-wrap olgrd9"><input type="text" name="olgrd9" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div></div>
-<div class="col-md-6">
-<div class="col-md-8"><span class="wpcf7-form-control-wrap olsub10"><input type="text" name="olsub10" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="10" /></span></div>
-<div class="col-md-4"><span class="wpcf7-form-control-wrap olgrd10"><input type="text" name="olgrd10" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div></div></div>
-<div class="row">
-<div class="col-md-12">
-<h4>G.C.E. Advanced Level</h4>
-</p></div></div>
-<div class="row">
-<div class="col-md-12">
-<div class="col-md-4"><span class="wpcf7-form-control-wrap alyear"><input type="text" name="alyear" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="Year" /></span></div>
-<div class="col-md-8"><span class="wpcf7-form-control-wrap alstream"><select name="alstream" class="wpcf7-form-control wpcf7-select wpcf7-validates-as-required" aria-required="true" aria-invalid="false"><option value="Stream of Study">Stream of Study</option><option value="Combined Maths">Combined Maths</option><option value="Bio Science">Bio Science</option><option value="Technology">Technology</option><option value="Art">Art</option><option value="Commerce">Commerce</option></select></span></div></div></div>
-<div class="row">
-<div class="col-md-6">
-<div class="col-md-8">Subject Passed</div>
-<div class="col-md-4">Grade</div></div>
-<div class="col-md-6">
-<div class="col-md-8">Subject Passed</div>
-<div class="col-md-4">Grade</div></div></div>
-<div class="row">
-<div class="col-md-6">
-<div class="col-md-8"><span class="wpcf7-form-control-wrap alsub1"><input type="text" name="alsub1" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="1" /></span></div>
-<div class="col-md-4"><span class="wpcf7-form-control-wrap algrd1"><input type="text" name="algrd1" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div></div>
-<div class="col-md-6">
-<div class="col-md-8"><span class="wpcf7-form-control-wrap alsub2"><input type="text" name="alsub2" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="2" /></span></div>
-<div class="col-md-4"><span class="wpcf7-form-control-wrap algrd2"><input type="text" name="algrd2" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div></div>
-<div class="col-md-6">
-<div class="col-md-8"><span class="wpcf7-form-control-wrap alsub3"><input type="text" name="alsub3" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="3" /></span></div>
-<div class="col-md-4"><span class="wpcf7-form-control-wrap algrd3"><input type="text" name="algrd3" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div></div>
-<div class="col-md-6">
-<div class="col-md-8"><span class="wpcf7-form-control-wrap alsub4"><input type="text" name="alsub4" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="4" /></span></div>
-<div class="col-md-4"><span class="wpcf7-form-control-wrap algrd4"><input type="text" name="algrd4" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div></div>
-<div class="col-md-6">
-<div class="col-md-8"><span class="wpcf7-form-control-wrap alsub5"><input type="text" name="alsub5" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="General Knowledge" /></span></div>
-<div class="col-md-4"><span class="wpcf7-form-control-wrap algrd5"><input type="text" name="algrd5" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div></div>
-<div class="col-md-6">
-<div class="col-md-8"><span class="wpcf7-form-control-wrap alsub6"><input type="text" name="alsub6" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="English" /></span></div>
-<div class="col-md-4"><span class="wpcf7-form-control-wrap algrd6"><input type="text" name="algrd6" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" /></span></div></div></div>
+          <div class="col-md-6">
+          <div class="col-md-8">
+          <span class="wpcf7-form-control-wrap olsub1">
+          <input type="text" name="olsub1" value="Mathematics" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" />
+          </span>
+          </div>
 
-    
-          
-          
   
     <div class="form-row">
     <button class="btn btn-primary" type="submit">Submit form</button>
