e7bb74e3d6b56b132b1abb8f2410034b70757b7d
keerththeejan <keerththeejan@gmail.com>
2019-09-17 12:16:10 +0530

    index title changes

---
+++ classtimetable.php
@@ -0,0 +1,40 @@
+
+
+
+<?php
+$title = "Home | SLGTI";
+include_once("config.php");
+include_once("head.php");
+include_once("menu.php");
+?>
+
+
+
+
+<a href="AddTimetable.php" button type="button" class="btn btn-success" role="button" aria-pressed="true" >Add New</button>
+</a>
+
+    
+
+
+
+
+
+
+
+
+<h1 class="text-right">
+ 
+ <div class="row"> 
+
+ <div class="col-12">
+ <a href="Timetable.php" button type="button" class="btn btn-success"  aria-pressed="true" >Back</button>
+
+</a>
+ 
+</h1>
+
+</div>
+</div>
+
+    <?php include_once("footer.php"); ?>
