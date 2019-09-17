82be1ee1bd1f90356896b2f7e6fec7adeb4e39bc
faheeeeeem <abdulwalith96@gmail.com>
2019-09-17 11:55:54 +0530

    update

--- OrderView.php
+++ OrderView.php
@@ -11,7 +11,7 @@ include_once("menu.php");
     <!-- end dont change the order-->
 <!--block 2 start my code here-->  
 
-   <div class= "row"> 
+<div class= "row"> 
    <div class ="col-sm-12 col-md-6 col-lg-6 container"><h1>"Perfect Food is Born of Perfect Order..."</h1></div>
    <div class ="col-sm-12 col-md-4 col-lg-4 container"><input type="date" class="form-control" id="datePic" aria-describedby="datePicHelp"></div>
 
@@ -26,54 +26,86 @@ include_once("menu.php");
       <th scope="col">Item Name</th>
       <th scope="col">Qty</th>
       <th scope="col">Total</th>
+>>>>>>> dcb158ad186279aa4453fc6732176e6f631f575f
 
-     
+<div class ="row">
+<div class="col-4"><h1>Order Information</h1></div>
+<div class="col-4"><input type="search" class="form-control ds-input" id="search-input" placeholder="Search..."
+ aria-label="Search for..." autocomplete="off" data-docs-version="4.3" 
+spellcheck="false" role="combobox" aria-autocomplete="list" aria-expanded="false" aria-owns="algolia-autocomplete-listbox-0"
+ dir="auto" style="position: relative; vertical-align: top;"></div>
+ <div class="col-4"><button type="button" class="btn btn-success">Go</button></div>
+ <table class="table table-borderless">
+  <thead>
+    <tr>
+    
      
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">Abdullah</th>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
+      <th scope="row">Order Number</th>
+      <td>15</td>
+      
     </tr>
     <tr>
-      <th scope="row">Sahan</th>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
+      <th scope="row">User Name</th>
+      <td>ABDULLAh</td>
+      
+    </tr>
+    
+  </tbody>
+</table>
+<table class="table table-borderless">
+  <thead>
+    <tr>
+      <th scope="col">Item Name</th>
+      <th scope="col">Item Qty</th>
+      <th scope="col">Amount</th>
+    
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Pittu</th>
+      <td>10</td>
+      <td>600</td>
+   
     </tr>
     <tr>
-      <th scope="row">Ikas</th>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
+      <th scope="row">Idiyappam</th>
+      <td>7</td>
+      <td>420</td>
+     
     </tr>
-    
-    
+    <tr>
+      <th scope="row"></th>
+      <td colspan="1" class="text-white bg-dark"><h5>Total Amount<h5></td>
+      <td  class="text-white bg-dark"><h5>1020<h5></td>
+    </tr>
   </tbody>
 </table>
 
 
 
-   
 
 
-   </div>
-    
-  
-
-    
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
 
     
   
