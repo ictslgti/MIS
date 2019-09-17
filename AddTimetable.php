d17cb982fdf320a540adcfd839e9a74511128ab9
keerththeejan <keerththeejan@gmail.com>
2019-09-17 11:04:48 +0530

    index title changes

--- AddTimetable.php
+++ AddTimetable.php
@@ -130,9 +130,7 @@ include_once("menu.php");
 
 
   
-  <div class="form-group row">
-    <div class="col-sm-10">
-      <button type="submit" class="btn btn-primary">Sign in</button>
+
     </div>
   </div>
 
