7b80ec28b3f94d4b113d6ea127866779b4a57fe1
keerththeejan <keerththeejan@gmail.com>
2019-09-17 10:54:22 +0530

    index title changes“

--- Timetable.php
+++ Timetable.php
@@ -32,22 +32,6 @@ include_once("menu.php");
           </div>
         </div>
 
-<div class="col-xl-3 col-sm-6 mb-3">
-          <div class="card text-white bg-warning o-hidden h-100">
-            <div class="card-body">
-              <div class="card-body-icon">
-                <i class="fa fa-fw fa-pie-chart"></i>
-              </div>
-              <div class="mr-5">Add Time table</div>
-            </div>
-            <a class="card-footer text-white clearfix small z-1" href="Timetableview.php">
-              <span class="float-left">View Details</span>
-              <span class="float-right">
-                <i class="fa fa-angle-right"></i>
-              </span>
-            </a>
-          </div>
-        </div>
 
           <div class="col-xl-3 col-sm-6 mb-3">
           <div class="card text-white bg-success o-hidden h-100">
@@ -55,7 +39,7 @@ include_once("menu.php");
               <div class="card-body-icon">
                 <i class="fa fa-fw fa-users"></i>
               </div>
-              <div class="mr-5">Add Exam Time table</div>
+              <div class="mr-5">Add Time table</div>
             </div>
             <a class="card-footer text-white clearfix small z-1" href="AddTimetable.php">
               <span class="float-left">View Details</span>
