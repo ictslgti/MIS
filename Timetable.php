b7911dc053ad95c5e41c5e82010749b9c5995faa
keerththeejan <keerththeejan@gmail.com>
2019-09-17 12:21:45 +0530

    index title changes

--- Timetable.php
+++ Timetable.php
@@ -18,7 +18,7 @@ include_once("menu.php");
 
         <div class="card text-white bg-primary o-hidden h-100">
             <div class="card-body">
-              <div class="card-body-icon">
+              <div class="card-body">
                 <i class="fa fa-fw fa-building"></i>
               </div>
               <div class="mr-5">Timetable Info</div>
@@ -39,9 +39,9 @@ include_once("menu.php");
               <div class="card-body-icon">
                 <i class="fa fa-fw fa-users"></i>
               </div>
-              <div class="mr-5">Add Time table</div>
+              <div class="mr-5">Add Class Time table</div>
             </div>
-            <a class="card-footer text-white clearfix small z-1" href="AddTimetable.php">
+            <a class="card-footer text-white clearfix small z-1" href="classtimetable.php">
               <span class="float-left">View Details</span>
               <span class="float-right">
                 <i class="fa fa-angle-right"></i>
@@ -52,7 +52,22 @@ include_once("menu.php");
 
 
 
-        
+        <div class="col-xl-3 col-sm-6 mb-3">
+          <div class="card text-white bg-success o-hidden h-100">
+            <div class="card-body">
+              <div class="card-body-icon">
+                <i class="fa fa-fw fa-users"></i>
+              </div>
+              <div class="mr-5">Add Exam Time table </div>
+            </div>
+            <a class="card-footer text-white clearfix small z-1" href="examtimetable.php">
+              <span class="float-left">View Details</span>
+              <span class="float-right">
+                <i class="fa fa-angle-right"></i>
+              </span>
+            </a>
+          </div>
+        </div>
         
 
 
