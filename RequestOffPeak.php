9b0068f2859468116dae2a74ad061d05dab6042a
sangeevan1 <sangeevan13@gmail.com>
2019-09-18 15:05:10 +0530

    sangee

--- RequestOffPeak.php
+++ RequestOffPeak.php
@@ -16,7 +16,7 @@ include_once("menu.php");
    
   
   <div class="form-row">
-    <div class="col-4" >
+    <div class="col-md-4 col-sm-12" >
     <br>
      
       <label for="text" class="font-weight-bolder" >Name of applicant :</label><br>
@@ -24,14 +24,14 @@ include_once("menu.php");
      
     </div>
     
-    <div class="col-4" >
+    <div class="col-md-4 col-sm-12" >
     <br>
     
     <label for="text" class="font-weight-bolder" >Registration No :</label><br>
     <input type="text" class="form-control" placeholder="" disabled>
     </div>
     
-    <div class="col-4" >
+    <div class="col-md-4 col-sm-12" >
     <br>
     
     <label for="text" class="font-weight-bolder"  >Department :</label><br>
@@ -39,19 +39,19 @@ include_once("menu.php");
     </div>
     </div>
     <div class="form-row">
-    <div class="col-4" >
+    <div class="col-md-4 col-sm-12" >
     <br>
    <label for="text" class="font-weight-bolder"  >Contact No :</label><br>
     <input type="text" class="form-control" placeholder="">
     </div>
     
 
-    <div class="col-4" >
+    <div class="col-md-4 col-sm-12" >
     <br>
    <label for="date" class="font-weight-bolder"  >Date :</label><br>
     <input type="date" class="form-control" placeholder="">
     </div>
-    <div class="col-4" >
+    <div class="col-md-4 col-sm-12" >
     <br>
    <label for="date" class="font-weight-bolder"  >Time :</label><br>
     <input type="time" class="form-control" placeholder="">
@@ -65,7 +65,7 @@ include_once("menu.php");
 
 
     <div class="row">
-    <div class="col-9">
+    <div class="col-md-9 col-sm-12">
     <br>
     
     <button type="button" class="btn btn-primary "><i class="fas fa-paper-plane"></i> Request to approval</button>
@@ -73,14 +73,19 @@ include_once("menu.php");
     
    
     
-    <div class="col-3">
+    <div class="col-md-1 col-sm-12">
     <br>
     <button type="button" class="btn btn-outline-dark btn-md ">Clear</button>
+
+</div>
+    <div class="col-md-4 col-sm-12">
+    <br>
+    <button type="button" class="btn btn-outline-info">Info</button>
     </div>
 
-    
    
-    
+
+   
 
 
    
@@ -91,66 +96,7 @@ include_once("menu.php");
  
 </form>
 
-<br><br>
 
-<div class="form-group container   p-3 mb-2 bg-light text-dark border border-primary rounded" >
-    <h4 class="h4 p-5 mb-5 bg-primary text-white rounded text-center  "><i class="fas fa-folder-open"></i>  Off-Peak Request Archives</h4>
-    <div class="table-responsive-sm">
-    <table class="table table-responsive-sm w-100">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Name of applicant</th>
-      <th scope="col">Registration No</th>
-      <th scope="col">Department</th>
-     <th colspan="3">Status</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>????</td>
-      <td>????</td>
-      <td>????</td>
-      <td>?????</td>
-     
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>????</td>
-      <td>????</td>
-      <td>????</td>
-      <td>?????</td>
-     
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>????</td>
-      <td>????</td>
-      <td>????</td>
-      <td>?????</td>
-      
-    </tr>
-    
-    <tr>
-      <th scope="row">3</th>
-      <td>????</td>
-      <td>????</td>
-      <td>????</td>
-      <td>?????</td>
-      
-    </tr>
-    
-  </tbody>
-</table>
-</div>
-
- 
-  </tbody>
-</table>
-
-
-    </div>
 
 
 
