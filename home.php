9f1113d35d86569bf4cd8ed0bd6f3afe97f52edc
sahangafoor <sahangafoor@gmail.com>
2019-09-20 09:11:52 +0400

    update file

---
+++ home.php
@@ -0,0 +1,116 @@
+<!-- BLOCK#1 START DON'T CHANGE THE ORDER -->
+<?php 
+$title = "Department Details | SLGTI" ;
+include_once("config.php"); 
+include_once("head.php"); 
+include_once("menu.php");
+ ?>
+<!-- END DON'T CHANGE THE ORDER -->
+<!-- Sidebar -->
+
+
+
+
+<div class ="row">
+<div class ="col-10  ">
+<ul class="nav justify-content-last">
+  <li class="nav-item">
+    <a class="nav-link create group text-secondary" href="#"><i class="fas fa-user-plus"></i>Create group</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link join group text-secondary" href="#"><i class="fas fa-users text-secondary"></i>Join group</a>
+  </li>
+
+</ul>
+
+
+   
+</div>
+
+
+
+                <div class ="col-2 p-3 p-3 mb-2 bg-dark text-white "><h5 class="text-center lg"  alt="50px"><i class="fas fa-users "></i></h5>
+                <div class ="row">
+
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
+                Gafoor Sahan</a>
+
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
+                Abdullah</a>
+
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
+                Faheem</a>
+
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
+                Nifras</a>
+
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
+                Kajan</a>
+
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
+                Thilogini</a>
+
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
+                Hanusiya</a>
+
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
+                Janani</a>
+
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
+                Jureesan</a>
+
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
+                Sanjeevan</a>
+
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
+                Keethijan</a>
+
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
+                Sarujan</a>
+
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
+                Mithusan</a>
+
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
+                Thurgisan</a>
+
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
+                Newsika</a>
+
+                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
+                Thuvarahan</a>
+
+                </div>
+                </div>   
+                </div>
+                </div>
+
+
+</div>
+
+
+
+
+
+
+
+ <!-- /#sidebar-wrapper -->
+ <!-- Page Content -->
+ 
+
+ 
+
+ 
+                
+
+                
+                
+                
+
+<!-- END YOUR CODER HERE -->
+
+    <!-- BLOCK#3 START DON'T CHANGE THE ORDER -->
+    <?php 
+    include_once("footer.php");
+    ?>
+    <!-- END DON'T CHANGE THE ORDER -->
