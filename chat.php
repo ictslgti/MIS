605080da206026c43ddb30d70f90e3bec79358c3
sahangafoor <sahangafoor@gmail.com>
2019-09-20 09:09:58 +0400

    update file

--- chat.php
+++ chat.php
@@ -9,85 +9,15 @@ include_once("menu.php");
 <!-- Sidebar -->
 
 
-
-
-<div class ="row">
-<div class ="col-10  ">
-<ul class="nav justify-content-last">
-  <li class="nav-item">
-    <a class="nav-link create group text-secondary" href="#"><i class="fas fa-user-plus"></i>Create group</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link join group text-secondary" href="#"><i class="fas fa-users text-secondary"></i>Join group</a>
-  </li>
-
-</ul>
-
-
-   
+<div class="jumbotron p-8 mb-2 bg-primary text-white ">
+<h1 class="display-1 text-center"><i class="far fa-comments"></i> STUDENT MESSENGER</h1>
+  <p class="text-center display-8 text-center">This is a student chating to share the informations. It using for the SLGTi students</p>
+  <a class="btn btn-outline-light float-right" href="home" role="button"  aria-disabled="true">getting start</a>
+  <h1 class="display-1 text-center">
+  <p class="text-center display-4 text-center">
+  
+  
 </div>
-
-
-
-                <div class ="col-2 p-3 p-3 mb-2 bg-dark text-white "><h5 class="text-center lg"  alt="50px"><i class="fas fa-users "></i></h5>
-                <div class ="row">
-
-                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
-                Gafoor Sahan</a>
-
-                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
-                Abdullah</a>
-
-                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
-                Faheem</a>
-
-                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
-                Nifras</a>
-
-                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
-                Kajan</a>
-
-                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
-                Thilogini</a>
-
-                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
-                Hanusiya</a>
-
-                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
-                Janani</a>
-
-                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
-                Jureesan</a>
-
-                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
-                Sanjeevan</a>
-
-                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
-                Keethijan</a>
-
-                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
-                Sarujan</a>
-
-                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
-                Mithusan</a>
-
-                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
-                Thurgisan</a>
-
-                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
-                Newsika</a>
-
-                <a class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white" href=""> <i class="fas fa-user-circle"></i>
-                Thuvarahan</a>
-
-                </div>
-                </div>   
-                </div>
-                </div>
-
-
-</div>
-
 
 
 
@@ -97,15 +27,9 @@ include_once("menu.php");
  <!-- /#sidebar-wrapper -->
  <!-- Page Content -->
  
-
  
 
  
-                
-
-                
-                
-                
 
 <!-- END YOUR CODER HERE -->
 
