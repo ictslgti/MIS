88b654dbd370524afcf5d35fbd349766d2ec3864
keerththeejan <keerththeejan@gmail.com>
2019-09-20 14:14:20 +0530

    index title changes

--- Timetable.php
+++ Timetable.php
@@ -8,88 +8,279 @@ include_once("head.php");
 include_once("menu.php");
 ?>
 
+
+
+
+
 <div class="row">
-<div class="row w-100">
+
+
+
+
+
         <div class="col-md-2">
-            <div class="card border-info mx-sm-1 p-3">
-            
+            <div class="card border-info mx-sm-1 p-1">
+            <div class="text-info text-center mt-1"><h4>Monday</h4></div>
+           
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="card border-success mx-sm-1 p-1">
+            <div class="text-info text-center mt-1"><h4>Tuesday</h4></div>
                
             </div>
         </div>
         <div class="col-md-2">
+            <div class="card border-danger mx-sm-1 p-1">
+                
+            <div class="text-info text-center mt-1"><h4>Wednesday</h4></div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="card border-warning mx-sm-1 p-1">
+                
+            <div class="text-info text-center mt-1"><h4>Thursday</h4></div>
+            </div>
+        </div>
+
+        <div class="col-md-2">
+            <div class="card border-warning mx-sm-1 p-1">
+            <div class="text-info text-center mt-1"><h4>Friday</h4></div>
+            </div>
+        </div>
+       </div>
+
+
+
+
+
+
+
+
+        <div class="row">
+        
+
+
+
+
+        <div class="col-md-2">
+            <div class="card border-info mx-sm-1 p-3">
+            
+            <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
+            </div>
+        </div>
+        <div class="col-md-2">
             <div class="card border-success mx-sm-1 p-3">
-               
+            <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
                
             </div>
         </div>
         <div class="col-md-2">
             <div class="card border-danger mx-sm-1 p-3">
                 
-               
+            <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
             </div>
         </div>
         <div class="col-md-2">
             <div class="card border-warning mx-sm-1 p-3">
                 
+            <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
+            </div>
+        </div>
+
+        <div class="col-md-2">
+            <div class="card border-warning mx-sm-1 p-3">
+            <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
+            </div>
+            </div>
+          </div>
+
+
+
+
+
+              <div class="row">
+
+
+
+
+
+
+        <div class="col-md-2">
+            <div class="card border-info mx-sm-1 p-3">
+            <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
+               
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="card border-success mx-sm-1 p-3">
+            <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
+               
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="card border-danger mx-sm-1 p-3">
+                
+            <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="card border-warning mx-sm-1 p-3">
+                
+            <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
+            </div>
+        </div>
+
+        <div class="col-md-2">
+            <div class="card border-warning mx-sm-1 p-3">
+            <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
+            </div>
+        </div>
+           </div>
+
+
+
+
+
+
+           <div class="row">
+
+
+
+
+
+        <div class="col-md-2">
+            <div class="card border-info mx-sm-1 p-3">
+            
+            <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="card border-success mx-sm-1 p-3">
+            <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
+               
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="card border-danger mx-sm-1 p-3">
+                
+            <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="card border-warning mx-sm-1 p-3">
+            <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
              
             </div>
         </div>
 
         <div class="col-md-2">
             <div class="card border-warning mx-sm-1 p-3">
-                
-               
-     </div>
-
-     
-  
-
-
-
-
-        </div>
-
-        </div>
-        <div class="col-md-2">
-            <div class="card border-info mx-sm-1 p-3">
-                
+            <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
             </div>
         </div>
-        <div class="col-md-2">
-            <div class="card border-success mx-sm-1 p-3">
-                
-            </div>
+      </div>
+
+
+
+
+      <div class="row">
+
+
+
+
+
+           <div class="col-md-2">
+             <div class="card border-info mx-sm-1 p-3">
+    
+             <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
         </div>
-        <div class="col-md-2">
-            <div class="card border-danger mx-sm-1 p-3">
-                
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card border-warning mx-sm-1 p-3">
-               
-            </div>
-        </div>
-
-        <div class="col-md-2">
-            <div class="card border-warning mx-sm-1 p-3">
-               
-
-
-            </div>
-        </div>
-
-
+          </div>
+            <div class="col-md-2">
+               <div class="card border-success mx-sm-1 p-3">
+       
+                   <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
+                     </div>
+              </div>
+                 <div class="col-md-2">
+                 <div class="card border-danger mx-sm-1 p-3">
         
+                <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
+              </div>
+                 </div>
+                 <div class="col-md-2">
+                          <div class="card border-warning mx-sm-1 p-3">
+        
+                  <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
+                  </div>
+               </div>
 
-</div>
-
-
-      
-</div>
-
-
-
+              <div class="col-md-2">
+                 <div class="card border-warning mx-sm-1 p-3">
+                 <div class="footer">
+                            <a href="#" class="Cbtn Cbtn-primary">View</a>
+                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
+                        </div>
+                   </div>
+                </div>
+                  </div>
 
 
 
