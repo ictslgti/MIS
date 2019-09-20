5e942a3346a6b37d453b430d6dfa1f852a3f8d62
sahangafoor <sahangafoor@gmail.com>
2019-09-20 08:54:54 +0400

    index title changes“

--- join.php
+++ join.php
@@ -12,7 +12,7 @@ include_once("menu.php");
 <div class="jumbotron p-8 mb-2 bg-primary text-white ">
 <h1 class="display-1 text-center"><i class="far fa-comments"></i> STUDENT MESSENGER</h1>
   <p class="text-center display-8 text-center">This is a student chating to share the informations. It using for the SLGTi students</p>
-  <a class="btn btn-outline-light float-right" href="#" role="button text-center">getting start</a>
+  <a class="btn btn-outline-light float-right" href="chat" role="button"  aria-disabled="true">getting start</a>
   <h1 class="display-1 text-center">
   <p class="text-center display-4 text-center">
   
