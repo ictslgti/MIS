250c92ade05773cb2773cbac1a768ece39354588
nawodajanani <nawoda19961225@gmail.com>
2019-09-26 09:00:28 +0530

    Add new link

---
+++ AddNotice.php
@@ -0,0 +1,48 @@
+<!--  bLOCK#1 start don't change the order-->
+<?php 
+$title =" Department Deatils| SLGTI";
+include_once("config.php");
+include_once("head.php");
+include_once("menu.php");
+?>
+<!-- end don't change the order-->
+
+
+
+<!-- bLOCK#2 start your code here & u can change -->
+<br>
+
+<div class="card-deck">
+<br>
+<br>
+    <div class="card text-white bg-dark mb-3">
+    <div class="card-header"><h1>Result</h1></div>
+        <div class="card-body">
+        <p class="card-text">
+        <a href class="noticeboard.php">
+        <h1>Add New Result<h1></a></p>
+        </div>
+    </div>
+  
+    <div class="card text-white bg-dark mb-3">
+    <div class="card-header"><h1>Events</h1></div>
+        <div class="card-body">
+        <div class="alert alert-dark"><a href class="#">Certificate Awarding Ceremony and Anniversary</a></div>
+        <div class="alert alert-dark" role="alert"><a href class="#">Christmas Day Celebration 2018</a></div>
+        <div class="alert alert-dark" role="alert"><a href class="#">Hon Minister's Visit</a></div>
+        <div class="alert alert-dark" role="alert"><a href class="#">Blood Donation and Helth Awareness Programme at SLGTI</a></div>
+        <div class="alert alert-dark" role="alert"><a href class="#">Other</a></div>
+      </div>
+    
+    </div>
+</div>
+
+   <!-- end your code here-->
+
+
+
+
+<!--bLOCK#3  start don't change the order-->
+    <?php include_once("footer.php");?>
+<!-- end don't change the order-->
+   
