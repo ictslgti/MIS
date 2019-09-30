f394a41b9940dc3fa2d7b6abf72fabe8ef296ae4
sahangafoor <sahangafoor@gmail.com>
2019-09-30 09:33:10 +0400

    update

--- create_group.php
+++ create_group.php
@@ -8,31 +8,76 @@ include_once("menu.php");
 <!-- END DON'T CHANGE THE ORDER -->
 <!-- Sidebar -->
 
+<div class="row">
 
-
-<div class ="row">
-<div class ="col-2 ">
-
-</div>
-
-
-<div class ="col-6">
-<div class="jumbotron-fluid ">
-  <div class="container ">
-    <h1 class="display-4 "><h4>Create an Group</h4>
-    <p class="lead">Don't have an group? it takes less than a minutes. I you already have an group.</p>
+<div class="col-8">
+<ul class="nav justify-content-last">
+<div class="container ">
+    <h1 class="display-1 "><h4>New Group</h4>
+    <small id="emailHelp" class="form-text text-muted">add subject</small>
+    <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1"></label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Type of group subject here">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your group with anyone else.</small>
   </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1"></label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Type of information">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your information with anyone else.</small>
+  </div>
+</ul>
+<button type="button" class="btn btn-outline-secondary float-right">Add</button>
+  </div> 
+</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="col-54">
+<div class="card p-3 mb-2 bg-dark text-white" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title"><h1><i class="fas fa-users float-center"></h1></i></h5>
+    <p class="card-text"><h5>Group Contact</h5></p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white"> <i class="fas fa-user-circle"></i> Gafoor Sahan</li>
+    <li class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white"><i class="fas fa-user-circle"></i> Abdullah</li>
+    <li class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white"><i class="fas fa-user-circle"></i> Faheem</li>
+    <li class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white"><i class="fas fa-user-circle"></i> Nifras</li>
+    <li class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white"><i class="fas fa-user-circle"></i> Kajan</li>
+    <li class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white"><i class="fas fa-user-circle"></i> Thilogini</li>
+    <li class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white"><i class="fas fa-user-circle"></i> Janani</li>
+    <li class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white"><i class="fas fa-user-circle"></i> Jureesan</li>
+    <li class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white"><i class="fas fa-user-circle"></i> Newsika</li>
+    <li class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white"><i class="fas fa-user-circle"></i> Sanjeevan </li>
+    <li class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white"><i class="fas fa-user-circle"></i> Keethijan</li>
+    <li class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white"><i class="fas fa-user-circle"></i> Sarujan</li>
+    <li class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white"><i class="fas fa-user-circle"></i> Mithusan</li>
+    <li class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white"><i class="fas fa-user-circle"></i> Thurgisan</li>
+    <li class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white"><i class="fas fa-user-circle"></i> Hanusiya</li>
+    <li class="list-group-item list-group-item-action p-3 mb-2 bg-dark text-white"><i class="fas fa-user-circle"></i> Thuvarahan</li>
+   
+  </ul>
+</div>
+</div>
 </div>
 </div>
 
-</div>
 
 
 
 
-<div class ="col-4">
-
-</div>
 
 
 
