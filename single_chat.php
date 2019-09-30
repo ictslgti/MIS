372e9a37ebf7a313d3428bbf986c312f693ee512
sahangafoor <sahangafoor@gmail.com>
2019-09-30 13:17:35 +0400

    update file“

---
+++ single_chat.php
@@ -0,0 +1,80 @@
+<!-- BLOCK#1 START DON'T CHANGE THE ORDER -->
+<?php 
+$title = "Department Details | SLGTI" ;
+include_once("config.php"); 
+include_once("head.php"); 
+include_once("menu.php");
+ ?>
+<!-- END DON'T CHANGE THE ORDER -->
+<!-- Sidebar -->
+
+
+
+<div class ="row">
+
+<div class ="col-4">
+
+</div>
+
+
+
+
+
+
+
+
+<div class ="col-4">
+  <div class="card-body p-3 mb-2 bg-dark text-white">
+  <p class="card-text  "><h5>single chat </h5></p>
+  </div>
+  <ul class="list-group list-group-flush ">
+    <li class="list-group-item list-group-item-action"> <i class="fas fa-user-circle"></i> Gafoor Sahan <small id="emailHelp" class="form-text text-muted float-center"> Hi how are you. I'm sahan and what about you</small></li>
+    <li class="list-group-item list-group-item-action"><i class="fas fa-user-circle"></i> Abdullah <small id="emailHelp" class="form-text text-muted float-center"> Hello boss you.</small></li>
+    <li class="list-group-item list-group-item-action"><i class="fas fa-user-circle"></i> Faheem <small id="emailHelp" class="form-text text-muted float-center"> feeling love.</small></li>
+    <li class="list-group-item list-group-item-action"><i class="fas fa-user-circle"></i> Nifras <small id="emailHelp" class="form-text text-muted float-center"> how are you.</small></li>
+    <li class="list-group-item list-group-item-action"><i class="fas fa-user-circle"></i> Kajan <small id="emailHelp" class="form-text text-muted float-center"> So what.</small></li>
+    <li class="list-group-item list-group-item-action"><i class="fas fa-user-circle"></i> Thilogini <small id="emailHelp" class="form-text text-muted float-center"> Hmmmmmmm.</small></li>
+    <li class="list-group-item list-group-item-action"><i class="fas fa-user-circle"></i> Janani <small id="emailHelp" class="form-text text-muted float-center">Tommorow will you come.</small></li>
+    <li class="list-group-item list-group-item-action"><i class="fas fa-user-circle"></i> Jureesan <small id="emailHelp" class="form-text text-muted float-center"> I'll play conter street.</small></li>
+    <li class="list-group-item list-group-item-action"><i class="fas fa-user-circle"></i> Newsika <small id="emailHelp" class="form-text text-muted float-center"> i call HOd.</small></li>
+    <li class="list-group-item list-group-item-action"><i class="fas fa-user-circle"></i> Sanjeevan <small id="emailHelp" class="form-text text-muted float-center"> Java is the best language.</small></li>
+    <li class="list-group-item list-group-item-action"><i class="fas fa-user-circle"></i> Keethijan <small id="emailHelp" class="form-text text-muted float-center"> what's going on.</small></li>
+    <li class="list-group-item list-group-item-action"><i class="fas fa-user-circle"></i> Sarujan <small id="emailHelp" class="form-text text-muted float-center"> What about you.</small></li>
+
+   
+  </ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+<div class ="col-4">
+fhf
+</div>
+</div>
+
+
+ <!-- /#sidebar-wrapper -->
+ <!-- Page Content -->
+ 
+
+ 
+
+ 
+                
+
+                
+                
+                
+
+<!-- END YOUR CODER HERE -->
+
+    <!-- BLOCK#3 START DON'T CHANGE THE ORDER -->
+    <?php 
+    include_once("footer.php");
+    ?>
+    <!-- END DON'T CHANGE THE ORDER -->
