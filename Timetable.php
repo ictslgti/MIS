2dceb474115d7c4998f3d42d90fc4951b71c0b09
keerththeejan <keerththeejan@gmail.com>
2019-10-01 13:20:46 +0530

    index title changes

--- Timetable.php
+++ Timetable.php
@@ -9,449 +9,197 @@ include_once("menu.php");
 ?>
 
 
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">period</th>
+      <th scope="col">Monday</th>
+      <th scope="col">Tuesday</th>
+      <th scope="col">Wednesday</th>
+      <th scope="col">Thursday</th>
+      <th scope="col">Friday</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">
+          
+      
+      <div class="card border-info mx-sm-3 p-2"></th>
 
 
-<div class=col-12>
+      <td>
+          
+      <div class="card border-info mx-sm-3 p-2">
+
+      </td>
+      <td> 
+          
+      
+      <div class="card border-info mx-sm-3 p-2">
 
 
-    <div class="row">
+      </td>
+      <td> 
+          
+      <div class="card border-info mx-sm-3 p-2">
 
 
-    <div class="col-md-12"></div>
-                 
-
-        <div class="col-md-2">
-            <div class="card panel-blue border-dark mx-sm-2 p-2">
-            <div class="btn   "><h4>Monday</h4></div>
-           
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card panel-blue border-dark mx-sm-2 p-2">
-            <div class="btn "><h4>Tuesday</h4></div>
-               
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card panel-blue border-dark mx-sm-2 p-2">
-                
-            <div class="btn "><h4>Wednesday</h4></div>
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card  panel-blue border-dark mx-sm-2 p-2">
-                
-            <div class="btn "><h4>Thursday</h4></div>
-            </div>
-        </div>
-
-        <div class="col-md-2">
-            <div class="card  panel-blue border-dark mx-sm-2 p-2">
-            <div class="btn "><h4>Friday</h4></div>
-            </div>
-        </div>
-       </div>
+      </td>
+      <td> 
+          
+      <div class="card border-info mx-sm-3 p-2">
 
 
+      </td>
+      <td> 
+          
+      <div class="card border-info mx-sm-3 p-2">
 
 
+      </td>
+      
+    </tr>
+    <tr>
+      <th scope="row">
+          
+      <div class="card border-info mx-sm-3 p-2">
 
 
+      </th>
+
+      <td>
 
 
-        <div class="row">
-        
+      <div class="card border-info mx-sm-3 p-2">
 
 
+      </td>
 
 
-        <div class="col-md-2">
-            <div class="card border-info mx-sm-3 p-2">
-            
-            <div class="footer">
-            <div class="card-header bg-primary text-center text-warning">
-
-            <h5 class="card-title">Special title </h5>
-               
-                </div>
-                
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-              
-    <br>
-
-                            <a href="AddTimetable.php" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Edit</a>
-                        </div>
-            </div>
-        </div>
-
-        <div class="col-md-2">
-            <div class="card border-success mx-sm-3 p-2">
-            <div class="footer">
-            <div class="card-header bg-primary text-center text-warning">
-                
-                <h5 class="card-title">Special title </h5>
-                </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-               
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card border-danger mx-sm-3 p-2">
-                
-            <div class="footer">
-            <div class="card-header bg-primary text-center text-warning">
-                
-            <h5 class="card-title">Special title </h5>
-            </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card border-warning mx-sm-3 p-2">
-                
-            <div class="footer">
-            <div class="card-header bg-primary text-center text-warning">
-                
-                <h5 class="card-title">Special title </h5>
-                </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-            </div>
-        </div>
-
-        <div class="col-md-2">
-            <div class="card border-warning mx-sm-3 p-2">
-            <div class="footer">
-            <div class="card-header bg-primary text-center text-warning">
-                
-                <h5 class="card-title">Special title </h5>
-                </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-            </div>
-            </div>
-          </div>
+      <td>
+          
+      <div class="card border-info mx-sm-3 p-2">
 
 
+      </td>
+
+      <td>
+          <div class="card border-info mx-sm-3 p-2">
 
 
+          </td>
+      <td>
+          
+      <div class="card border-info mx-sm-3 p-2">
 
-              <div class="row">
+      </td>
 
-
-
-
-
-
-        <div class="col-md-2">
-            <div class="card border-info mx-sm-3 p-2">
-                
-            <div class="footer">
-            <div class="card-header bg-primary text-center text-warning">
-                
-            <h5 class="card-title">Special title </h5>
-            </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-               
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card border-success mx-sm-3 p-2">
-            <div class="footer">
-            <div class="card-header bg-primary text-center text-warning">
-                
-                <h5 class="card-title">Special title </h5>
-                </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-               
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card border-danger mx-sm-3 p-2">
-                
-            <div class="footer">
-            <div class="card-header bg-primary text-center text-warning">
-                
-                <h5 class="card-title">Special title </h5>
-                </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card border-warning mx-sm-3 p-2">
-                
-            <div class="footer">
-            <div class="card-header bg-primary text-center text-warning">
-                
-                <h5 class="card-title">Special title </h5>
-                </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-            </div>
-        </div>
-
-        <div class="col-md-2">
-            <div class="card border-warning mx-sm-3 p-2">
-            <div class="footer">
-            <div class="card-header bg-primary text-center text-warning">
-                
-                <h5 class="card-title">Special title </h5>
-                </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-            </div>
-        </div>
-           </div>
+      <td>
+          
+      <div class="card border-info mx-sm-3 p-2">
 
 
+      </td>
+
+    </tr>
 
 
+    <tr>
+      <th scope="row">
+          
+      <div class="card border-info mx-sm-3 p-2">
 
 
-           <div class="row">
+      </th>
+
+      <td>
+          
+      <div class="card border-info mx-sm-3 p-2">
 
 
+      </td>
+
+      <td>
+          
+      <div class="card border-info mx-sm-3 p-2">
 
 
+      </td>
 
-        <div class="col-md-2">
-            <div class="card border-info mx-sm-3 p-2">
-            
-            <div class="footer">
-            <div class="card-header bg-primary text-center text-warning">
-                
-                <h5 class="card-title">Special title </h5>
-                </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card border-success mx-sm-3 p-2">
-            <div class="footer">
-            <div class="card-header bg-primary text-center text-warning">
-                
-            <h5 class="card-title">Special title </h5>
-            </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-               
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card border-danger mx-sm-3 p-2">
-                
-            <div class="footer">
-            <div class="card-header bg-primary text-center text-warning">
-                
-                <h5 class="card-title">Special title </h5>
-                </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card border-warning mx-sm-3 p-2">
-            <div class="footer">
-            <div class="card-header bg-primary text-center text-warning">
-                
-                <h5 class="card-title">Special title </h5>
-                </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-             
-            </div>
-        </div>
-
-        <div class="col-md-2">
-            <div class="card border-warning mx-sm-3 p-2">
-            <div class="footer">
-            <div class="card-header bg-primary text-center text-warning">
-                
-                <h5 class="card-title">Special title </h5>
-                </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-            </div>
-        </div>
-      </div>
+      <td>
+          
+      <div class="card border-info mx-sm-3 p-2">
 
 
+      </td>
+
+      <td>
+          
+      
+      <div class="card border-info mx-sm-3 p-2">
 
 
-      <div class="row">
+      </td>
+
+      <td>
+          
+      <div class="card border-info mx-sm-3 p-2">
 
 
-
-
-
-           <div class="col-md-2">
-             <div class="card border-info mx-sm-3 p-2">
+      </td>
     
-             <div class="footer">
-             <div class="card-header bg-primary text-center text-warning">
-                
-                <h5 class="card-title">Special title </h5>
-                </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-        </div>
-          </div>
-            <div class="col-md-2">
-               <div class="card border-success mx-sm-3 p-2">
-       
-                   <div class="footer">
-                   <div class="card-header bg-primary text-center text-warning">
-                
-            <h5 class="card-title">Special title </h5>
-            </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-                     </div>
-              </div>
-                 <div class="col-md-2">
-                 <div class="card border-danger mx-sm-3 p-2">
-        
-                <div class="footer">
-                <div class="card-header bg-primary text-center text-warning">
-                
-                <h5 class="card-title">Special title </h5>
-                </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-              </div>
-                 </div>
-                 <div class="col-md-2">
-                          <div class="card border-warning mx-sm-3 p-2">
-        
-                  <div class="footer">
-                  <div class="card-header bg-primary text-center text-warning">
-                
-                <h5 class="card-title">Special title </h5>
-                </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <br>
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn Cbtn-danger">Delete</a>
-                        </div>
-                  </div>
-               </div>
+    </tr>
+    <tr>
+      <th scope="row">
+          
+      <div class="card border-info mx-sm-3 p-2">
 
 
 
+      </th>
+
+      <td>
+          
+      <div class="card border-info mx-sm-3 p-2">
+
+
+      </td>
+
+      <td>
+          
+      <div class="card border-info mx-sm-3 p-2">
+
+
+      </td>
+
+      <td>
+          
+      <div class="card border-info mx-sm-3 p-2">
+
+
+      </td>
+
+      <td>
+          
+      <div class="card border-info mx-sm-3 p-2">
+
+
+      </td>
+
+      <td>
+          
+      <div class="card border-info mx-sm-3 p-2">
 
 
 
-
-
-              <div class="col-md-2">
-                 <div class="card  panel-yellowgreen;   border-warning mx-sm-3 p-2">
-                     
-                 <div class="footer">
-
-
-                 <div class="card-header bg-primary text-center text-warning">
-                
-                <h5 class="card-title">Special title </h5>
-                </div>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-   
-    <br>
-
-
-
-
- <div class="card-footer bg-info text-right text-danger">
-                            <a href="#" class="Cbtn Cbtn-primary">View</a>
-                            <a href="#" class="Cbtn  Cbtn-danger">Delete</a>
-    </div>
-                        </div>
-                   </div>
-                </div>
-                  </div>
-
-
-
-</div>
+      </td>
+    
+    </tr>
+  </tbody>
+</table>
         <?php include_once("footer.php"); ?>
+
+
+
+
