964ca39d9427bb0ba83ef8868a30e5155a903ae8
abdullahart1991 <artabdullah31@gmail.com>
2019-10-03 12:13:16 +0530

    update

---
+++ AcademicYear.php
@@ -0,0 +1,76 @@
+<!-- BLOCK#1 START DON'T CHANGE THE ORDER -->
+<?php 
+$title = "Department Details | SLGTI" ;
+include_once("config.php"); 
+include_once("head.php"); 
+include_once("menu.php");
+
+ ?>
+<!-- END DON'T CHANGE THE ORDER -->
+
+<!-- BLOCK#2 START YOUR CODER HERE -->
+<div class="shadow  p-3 mb-5 bg-white rounded">
+
+        <div class="highlight-blue">
+            <div class="container">
+                <div class="intro">
+                    <h1 class="display-4 text-center">SLGTI Academic Year</h1>
+                    
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href="AddAcademicYear" button type="button" class="btn btn-success"><i class="fas fa-plus"></i>&nbsp;Add Academic Year </a>
+
+
+
+<!-- <h1 class="col text-center">Department Details</h1> -->
+<br><br>
+<table class="table table-hover">
+  <thead class="thead-dark">
+ 
+    <tr >
+      <th scope="col">Academic Year</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+ 
+  <tr class="table-light">
+      
+  
+      <td>2018/2019</td>
+      
+  
+    </tr>
+    <tr class="table-light">
+     
+     
+      <td>2016/2017</td>
+     </tr>
+    <tr class="table-light">
+    
+     
+      <td>2014/2015</td>
+       </tr>
+    <tr class="table-light ">
+     
+     
+      <td>2013/2012</td>
+       </tr>
+    
+    
+    </tr>
+   
+  </tbody>
+  </form>
+</table>
+</div>
+<!-- END YOUR CODER HERE -->
+
+    <!-- BLOCK#3 START DON'T CHANGE THE ORDER -->
+    <?php 
+    include_once("footer.php");
+    ?>
+    <!-- END DON'T CHANGE THE ORDER -->
