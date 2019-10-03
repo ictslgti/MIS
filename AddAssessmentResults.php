4905130351775a51a1e9ce571e09b80156fcc798
Midhushan <mithunanirudh@gmail.com>
2019-10-03 12:04:18 +0530

    ok

--- AddAssessmentResults.php
+++ AddAssessmentResults.php
@@ -318,6 +318,39 @@ $title = "Examinations | SLGTI";
                 <br>
             </div>
 
+            <!-- sinna table thodakkam -->
+
+            <table class="table table-sm">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Student Index Number</th>
+                        <th scope="col">Full Name</th>
+                        <th scope="col">Module Marks</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+            <!-- small view table end -->
+
             <!-- main table container end below -->
         </div>
         <!-- end mode  -->
