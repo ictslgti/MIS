e891d1c52044846f959f1f1488b58782ae5cab4e
abdullahart1991 <artabdullah31@gmail.com>
2019-10-03 12:15:11 +0530

    update

---
+++ AddNewBatch.php
@@ -0,0 +1,56 @@
+<!-- BLOCK#1 START DON'T CHANGE THE ORDER -->
+<?php 
+$title = "Department Details | SLGTI" ;
+include_once("config.php"); 
+include_once("head.php"); 
+include_once("menu.php");
+ ?>
+<!-- END DON'T CHANGE THE ORDER -->
+
+<!-- BLOCK#2 START YOUR CODER HERE -->
+<div class="shadow p-3 mb-5 bg-white rounded">
+
+        <div class="highlight-blue">
+            <div class="container">
+                <div class="intro">
+                    <h1 class="display-3 text-center">Add New Batch</h1>
+                    
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+<table class="table table-hover">
+  <thead class="thead-dark">
+ 
+    <tr >
+    <th scope="col">Batch_ID</th>
+      <th scope="col">Course Name</th>
+      <th scope="col">Academic Year</th>
+   
+     
+      
+    </tr>
+  </thead>
+  <tbody>
+ 
+  
+
+
+  </tbody>
+  </form>
+</table>
+<a href="" class="btn btn-success" role="button" aria-pressed="true">Add</a>
+<a href="" class="btn btn-success" role="button" aria-pressed="true">Delete</a>
+<a href="" class="btn btn-success" role="button" aria-pressed="true">Edit</a>
+<a href="BatchDetails" class="btn btn-primary" role="button" aria-pressed="true">Back</a>
+<!-- END YOUR CODER HERE -->
+
+    <!-- BLOCK#3 START DON'T CHANGE THE ORDER -->
+    <?php 
+    include_once("footer.php");
+    ?>
+    <!-- END DON'T CHANGE THE ORDER -->
