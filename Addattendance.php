123f4ac47af97cacab5109a29667c47115f2df2f
MohanrajHanushiya <mohanrajsripriya@gmail.com>
2019-10-04 11:55:47 +0530

    Addattendance changes“

--- Addattendance.php
+++ Addattendance.php
@@ -20,36 +20,7 @@ include_once ("attendancenav.php");
       <div class="row">
         <div class="col-md-9">Attendance List</div>
         <div class="col-md-3" align="right">
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Report
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <div class="input-daterange">
-      <input class="form-control" id="date" name="date" placeholder="From Date" type="text"/>
-            <span id="error_from_date" class="text-danger"></span>
-            <br />
-            <input class="form-control" id="date" name="date" placeholder="From Date" type="text"/>
-            <span id="error_to_date" class="text-danger"></span>
-          </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-success" data-dismiss="modal">Create Report</button>
-        <button type="button" class="btn btn-danger">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
+        
 
           <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#myModal">Add</button>
          </div>
