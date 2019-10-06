7d5e55743d6ecef10c1fbf7f0f2a9b769d6618fa
GAJANGTR9060 <sivgajan96in@gmail.com>
2019-10-06 16:53:45 +0530

    design

--- Staff.php
+++ Staff.php
@@ -8,73 +8,39 @@ include_once("menu.php");
 <!--END DON'T CHANGE THE ORDER-->
 
 <!--BLOCK#2 START YOUR CODE HERE -->
-<html>
 
-<head>
-   
-</head>
-<body>
-<br><br>
-      <div class="row">
-      <div class="col form-group  container p-3 mb-2 col-4 " >
-    <h1 ><i class="fas fa-file-alt"></i>STAFF INFO</h1></div>
-    <div class="col form-group  container p-3 mb-2 col-4 " ></div>
-    <div class="col form-group  container p-3 mb-2 col-4 " >
-  
-    
-    
-        
+<!-- STAFF info design  -->
+<div class="row">
+    <div class="col container pt-3 col-4 " >
+      <h1 ><i class="fas fa-file-alt"></i>STAFF INFO</h1>
     </div>
-    
-    </div>
-   </div>
-   <br><br>
-   <div class="row">
+    <div class="col container  col-4 " ></div>
+</div>
+
+<div class="row">
    <div class="col-sm-12" >
-   <hr color ="black" style="height:1px;">
+      <hr color ="black" style="height:1px;">
    </div>
-  </div>
-  <form>
-<div class="form-row pl-3">
-    <div class="col-3" >
-    <div class="form-row align-items-center">
-      
-       <select class="custom-select  mr-sm-2" id="inlineFormCustomSelect">
-            <option selected>Department Name</option>
-      </select>
-    </div><br></div>
+</div>
 
-    <div class="col-2" >
-    <div class="form-row align-items-center">
-       
-       <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-            <option selected>Academic_Year</option>
-      </select>
-    </div></div><br><br><br>
+<div class="row pt-2">
+  <table class="table">
+    <thead class="thead-light">
+      <tr>
+        <th>StaffID</th>
+        <th>Name</th>
+        <th>Position</th>
+        <th>Department Name</th>
+        <th>Phone Number</th>
+      </tr>
+    </thead>
+    <tbody>
+    </tbody>
+  </table>
+</div>
 
-    <div class="col-1" >
-    <div class="form-row align-items-center">
-    <button type="button" class="btn btn-outline-primary align= right">Search</button>
-    </div><br></div></div>
-    <table class="table table-bordered">
-  <thead >
-    <tr>
-      <th scope="col">Staff_ID</th>
-      <th scope="col">Staff_Name</th>
-      <th scope="col">Module_Name</th>
-      <th scope="col">Position</th>
-      <th scope="col">Academic_Year</th>
-      <th scope="col">Department</th>
-    </tr>
-  </thead>
-  <tbody>
-    
-    <tr>
-      
-    </tr>
-  </tbody>
-</table>
-   
+
+
 <!--END OF YOUR COD-->
 
 <!--BLOCK#3 START DON'T CHANGE THE ORDER-->
