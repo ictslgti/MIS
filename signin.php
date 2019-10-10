bdc57a35c2e9a9e440da96af5e10aeb19ed30148
HOMEPC-ACHCHUTH\ACHCHUTHAN <achch1990@gmail.com>
2019-10-10 21:57:28 +0530

    DB data

--- signin.php
+++ signin.php
@@ -9,25 +9,24 @@ $msg = null;
 if (isset($_POST['SignIn']) && !empty($_POST['username']) && !empty($_POST['password'])) {
   $username = htmlspecialchars($_POST['username']);
   $password = htmlspecialchars($_POST['password']);
-  $_SESSION['user_name'] = $username;
-
-  if ($username == 'admin') {
-    $_SESSION['user_type'] = 'ADM';
-    $_SESSION['department_code'] = 'ALL'; // null or values(ICT,AUT,COT...)
+  $sql = "SELECT * FROM `user` WHERE `user_name`='$username' AND `user_password_hash`='$password'";
+  $result = mysqli_query($con,$sql);
+  if(mysqli_num_rows($result)==1){
+    $row = mysqli_fetch_assoc($result);
+    $_SESSION['user_type'] = $row['user_table'];
+    $_SESSION['user_name'] = $username;   
+    if($row['user_table']=='staff'){
+      $sql_u = "SELECT * FROM `staff` WHERE `staff_id` = '$username'";
+      $result_u = mysqli_query($con,$sql_u);
+      if(mysqli_num_rows($result_u)==1){
+        $row_u = mysqli_fetch_assoc($result_u);
+       $_SESSION['department_code'] = $row_u['department_id'];
+      }
+    }else{
+       echo  $_SESSION['department_code'] = 'ALL';
+    }
     header("Location: index.php");
-  } else if ($username == 'student') {
-    $_SESSION['user_type'] = 'STU';
-    $_SESSION['department_code'] = 'ICT'; // null or values(ICT,AUT,COT...)
-    header("Location: index.php");
-  } else if ($username == 'lecturer') {
-    $_SESSION['user_type'] = 'LEC';
-    $_SESSION['department_code'] = 'ICT'; // null or values(ICT,AUT,COT...)
-    header("Location: index.php");
-  } else if ($username == 'hod') {
-    $_SESSION['user_type'] = 'HOD';
-    $_SESSION['department_code'] = 'ICT'; // null or values(ICT,AUT,COT...)
-    header("Location: index.php");
-  } else {
+  }else {
     $msg = 'Invalid login, please try again';
   }
 }
@@ -46,40 +45,44 @@ if (isset($_GET['signout'])) {
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/signin.css">
-  <link href="css/all.min.css" rel="stylesheet">
-  <link href="css/simple-sidebar.css" rel="stylesheet">
-  <title><?php echo $title; ?></title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/signin.css">
+    <link href="css/all.min.css" rel="stylesheet">
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+    <title><?php echo $title; ?></title>
 </head>
 
 <body>
-  <div class="d-flex" id="wrapper">
-    <!--BLOCK#2 START YOUR CODE HERE -->
-    <div class="text-center signin">
-      <form class="form-signin" action="#" method="post">
-        <img class="mb-4" src="img/logo-1.png" alt="" height="100">
-        <?php
+    <div class="d-flex" id="wrapper">
+        <!--BLOCK#2 START YOUR CODE HERE -->
+        <div class="text-center signin">
+            <form class="form-signin" action="#" method="post">
+                <img class="mb-4" src="img/logo-1.png" alt="" height="100">
+                <?php
         if (!empty($msg))
           echo '<div class="alert alert-danger" >' . $msg . '</div>';
         ?>
-        <h1 class="mb-3">Sign in to MIS</h1>
-        <label for="inputEmail" class="sr-only">Username</label>
-        <input type="text" id="inputEmail" class="form-control" name="username" placeholder="[admin,student,lecturer,hod]" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" name="password" placeholder="1234" required>
-        <button type="submit" name="SignIn" class="btn btn-lg btn-primary btn-block">Sign in</button>
-        <div>
-          <a href="#">Forgot password?</a>
-        </div>
+                <h1 class="mb-3">Sign in to MIS</h1>
+                <label for="inputEmail" class="sr-only">Username</label>
+                <input type="text" id="inputEmail" class="form-control" name="username"
+                    placeholder="[achchuthan,2025ICT5IT01]" required autofocus>
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input type="password" id="inputPassword" class="form-control" name="password" placeholder="1234"
+                    required>
+                <button type="submit" name="SignIn" class="btn btn-lg btn-primary btn-block">Sign in</button>
+                <div>
+                    <a href="#">Forgot password?</a>
+                </div>
 
-      </form>
+            </form>
+        </div>
     </div>
-  </div>
-  <body>
+
+    <body>
+
 </html>
