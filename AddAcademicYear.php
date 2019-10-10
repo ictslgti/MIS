a6d650e7bbbf3d14cec50608d99e10694538e605
abdullahart1991 <artabdullah31@gmail.com>
2019-10-10 13:25:30 +0530

    update

--- AddAcademicYear.php
+++ AddAcademicYear.php
@@ -28,23 +28,19 @@ include_once("menu.php");
  
     <tr >
     <label for="inputPassword5">Add Academic Year</label>
-<input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
-
-     
-  
-    
-    
+    <input class="form-control" type="text" placeholder="Academic Year">
     </tr>
    
   </tbody>
   </form>
 </table>
 </div>
+<div class="  p-3 mb-5">
 <a href="" class="btn btn-success" role="button" aria-pressed="true">Add</a>
 <a href="" class="btn btn-success" role="button" aria-pressed="true">Delete</a>
 <a href="" class="btn btn-success" role="button" aria-pressed="true">Edit</a>
 <a href="AcademicYear" class="btn btn-primary" role="button" aria-pressed="true">Back</a>
-<br>
+</div>
 <!-- END YOUR CODER HERE -->
 
     <!-- BLOCK#3 START DON'T CHANGE THE ORDER -->
