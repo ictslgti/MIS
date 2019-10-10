f05566ac892250f0b8d161551ae6bfe54b60f3c1
achchuthany <achch1990@gmail.com>
2019-10-10 09:35:55 +0530

    DB connection

--- config.php
+++ config.php
@@ -1,3 +1,17 @@
 <?php
 session_start();
+
+
+define('DB_HOST','mis.achchuthan.org');
+define('DB_USER','misuser');
+define('DB_PASS','mIs@SlgT1');
+define('DB_NAME','mis');
+$con=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+if (mysqli_connect_errno()){
+      echo "Failed to connect to MySQL: " . 		
+      mysqli_connect_error();
+  }else{
+    echo "Connected successfully";
+  }
+
 ?>
