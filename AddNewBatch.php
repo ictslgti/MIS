ff309298139606db689ed5e0f7237ddb9aa70bed
abdullahart1991 <artabdullah31@gmail.com>
2019-10-10 11:53:11 +0530

    update changes

--- AddNewBatch.php
+++ AddNewBatch.php
@@ -53,6 +53,7 @@ include_once("menu.php");
 <a href="" class="btn btn-success" role="button" aria-pressed="true">Delete</a>
 <a href="" class="btn btn-success" role="button" aria-pressed="true">Edit</a>
 <a href="BatchDetails" class="btn btn-primary" role="button" aria-pressed="true">Back</a>
+<br>
 <!-- END YOUR CODER HERE -->
 
     <!-- BLOCK#3 START DON'T CHANGE THE ORDER -->
