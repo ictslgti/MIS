19ed40f4ea94a1dc3461c9e7cfcf91be09e70db5
GAJANGTR9060 <sivgajan96in@gmail.com>
2019-10-11 18:52:51 +0530

    php

--- AddStaff.php
+++ AddStaff.php
@@ -57,10 +57,10 @@ if(isset($_POST['Add'])){
         ';
       }
       else{
-        echo "Error".$sql."<br>".mysqli_error($con);
+        
         echo '
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <strong>'.$StaffName.'</strong> You should check in on some of those fields below.
+        <strong>'.$StaffName.'</strong> echo "Error".$sql."<br>".mysqli_error($con);
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
         </button>
