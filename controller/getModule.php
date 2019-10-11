d50ab3472c1fb9ede17e776bf4f1b0848627f6e4
Midhushan <mithunanirudh@gmail.com>
2019-10-11 14:20:53 +0530

    edited

--- controller/getModule.php
+++ controller/getModule.php
@@ -13,11 +13,11 @@ if(isset($_POST['course'])){
 if(isset($_POST['semister'])){
     $id = $_POST['semister'];
     echo '<option value="null"  selected disabled>--Select Module--</option>';
-    echo '<option value="M1">Course '.$id.' Module 1</option>';
-    echo '<option value="M2">Course '.$id.' Module 2</option>';
-    echo '<option value="M3">Course '.$id.' Module 3</option>';
-    echo '<option value="M4">Course '.$id.' Module 4</option>';
-    echo '<option value="M5">Course '.$id.' Module 5</option>';
+    echo '<option value="M1">semester '.$id.' Module 1</option>';
+    echo '<option value="M2">semester '.$id.' Module 2</option>';
+    echo '<option value="M3">semester '.$id.' Module 3</option>';
+    echo '<option value="M4">semester '.$id.' Module 4</option>';
+    echo '<option value="M5">semester '.$id.' Module 5</option>';
 
 }
 
