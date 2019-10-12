f12b0644c7c43bea17de5a163799aa7f7c33536c
GAJANGTR9060 <sivgajan96in@gmail.com>
2019-10-12 22:49:19 +0530

    search

--- AddStaff.php
+++ AddStaff.php
@@ -89,7 +89,7 @@ if(isset($_POST['Add'])){
     <div class="col-sm-3 pt-4"> 
       <form class="form-inline" method="GET">
         <input class="form-control mr-2" type="search" name="edit" placeholder="Staff ID">  
-        <button class="btn btn-outline-success my-2 my-sm-0"  onclick="Search()">Search</button>
+        <button type="button" class="btn btn-outline-success my-2 my-sm-0"  onclick="Search()">Search</button>
       </form>
     </div>  
 </div>
