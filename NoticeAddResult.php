7a6ac76caa14da37f989002a69c78331cf0354b7
nawodajanani <nawoda19961225@gmail.com>
2019-10-16 09:50:18 +0530

    link

--- NoticeAddResult.php
+++ NoticeAddResult.php
@@ -10,126 +10,98 @@ include_once("menu.php");
 
 
 <!-- bLOCK#2 start your code here & u can change -->
-<br>
-
-<div class="row border border-light shadow p-3 mb-5 bg-white rounded">
-          <div class="col">
-          <br>
-          <br>
-            <blockquote class="blockquote text-center">
-                <h1 class="display-4">ADD RESULT</h1> 
-                <p class="mb-0">Srilanka German Training Institute</p>
-                <footer class="blockquote-footer">Result Description<cite title="Source Title"></cite></footer>
-            </blockquote>
-          </div>
+<hr>
+<div class="alert bg-dark text-white" role="alert">
+  <h1>Add New Result</h1>
 </div>
-
-<form method="post">
-<div class="   mr-5 ml-5 mt-5 mb-5">
-
-    <div class="input-group mb-3 ">
-        <div class="input-group-prepend">
-            <label class="input-group-text" for="inputGroupSelect01"> 
-            <i class="fas fa-award"></i> </i>&nbsp;&nbsp;Result&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-        </div>
-        <input type="text" class="form-control" id="inputPassword2" name="event_name" placeholder="Result">
-    </div> 
-
-
-
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-          <label class="input-group-text" for="inputGroupSelect01"><i class="fas fa-university"></i>&nbsp;&nbsp;Department&nbsp;&nbsp;&nbsp;&nbsp;</label>
-      </div>
-        <select class="custom-select" id="inputGroupSelect01">
-          <option selected>Select Your Department...</option>
-          <option value="1">Electrical & Electronic Technology Department</option>
-          <option value="2">Construction Technology Department</option>
-          <option value="3">Information & Communications Technology Department</option>
-          <option value="4">Mechanical Technology Department</option>
-          <option value="5">Food Technology Department</option>
-          <option value="6">Automotive & Agricultural Technology Department</option>
-        </select>
+<hr>
+<div class="form-group row">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">&nbsp;&nbsp;&nbsp;Type</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputEmail3"required placeholder="Result...">
     </div>
+  </div>
 
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-          <label class="input-group-text" for="inputGroupSelect01">&nbsp;&nbsp;Batch No&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-      </div>
-        <select class="custom-select" id="inputGroupSelect01">
-          <option selected>Select Your Batch No...</option>
-          <option value="1">Batch 01</option>
-          <option value="2">Batch 02</option>
-          <option value="3">Batch 03</option>
-        </select>
-    </div>
 
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-          <label class="input-group-text" for="inputGroupSelect01">&nbsp;&nbsp;Level&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-      </div>
-        <select class="custom-select" id="inputGroupSelect01">
-          <option selected>Select Your Level...</option>
-          <option value="1">Level 04</option>
-          <option value="2">Bridging</option>
-          <option value="3">Level 05</option>
-          <option value="4">Level 06</option>
-        </select>
-    </div>
+  <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">&nbsp;&nbsp;&nbsp;Department</label>
+    <div class="col-sm-10">
+	<select id="inputState" class="form-control">
+      <option selected>Select the Department...</option>
+      <option>Electrical & Electronic Technology Department</option>
+      <option>Construction Technology Department</option>
+      <option>Information & Communications Technology Department</option>
+      <option>Mechanical Technology Department</option>
+      <option>Food Technology Department</option>
+      <option>Automotive & Agricultural Technology Department</option>
+    </select>
+  </div>
+</div>
+  <div class="form-group row">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">&nbsp;&nbsp;&nbsp;Academic Year</label>
+    <div class="col-sm-10">
+    <select id="inputState" class="form-control">
+      <option selected>Select the Academic Year...</option>
+      <option>2016/2017</option>
+      <option>2017/2018</option>
+      <option>2018/2019</option>
+    </select>
+  </div>
+  </div>
 
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-          <label class="input-group-text" for="inputGroupSelect01"> <i class="fab fa-audible"></i>&nbsp;&nbsp;Modules&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-      </div>
-        <select class="custom-select" id="inputGroupSelect01">
-        <option selected>Select the Module...</option>
-              <option value="1">Module 01</option>
-              <option value="2">Module 02</option>
-              <option value="3">Module 03</option>
-              <option value="4">Module 04</option>
-              <option value="4">Module 05</option>
-              <option value="4">Module 06</option>
-              <option value="4">Module 07</option>
-              <option value="4">Module 08</option>
-              <option value="4">Module 09(Manage Workplace Information)</option>
-              <option value="4">Module 10(Manage Workplace Communication)</option>
-            </select>
-        </select>
-    </div>
+  <div class="form-group row">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">&nbsp;&nbsp;&nbsp;Course</label>
+    <div class="col-sm-10">
+    <select id="inputState" class="form-control">
+      <option selected>Select Your Course...</option>
+      <option>Level 04</option>
+      <option>Bridging</option>
+      <option>Level 05</option>
+      <option>Level 06</option>
+    </select>
+  </div>
+  </div>
+ 
+  <div class="form-group row">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">&nbsp;&nbsp;&nbsp;Modules</label>
+    <div class="col-sm-10">
+    <select id="inputState" class="form-control">
+      <option selected>Select the Module...</option>
+      <option>Module 01</option>
+      <option>Module 02</option>
+      <option>Module 03</option>
+      <option>Module 04</option>
+      <option>Module 05</option>
+      <option>Module 06</option>
+      <option>Module 07</option>
+      <option>Module 08</option>
+      <option>Module 09(Manage Workplace Information)</option>
+      <option>Module 10(Manage Workplace Communication)</option>
+    </select>
+  </div>
+  </div>
+
+ 
   
-    <!-- <div class="input-group mb-3 ">
-        <div class="input-group-prepend">
-            <label class="input-group-text" for="inputGroupSelect01"> 
-            <i class="fas fa-plus"></i>  </i>&nbsp;&nbsp;Add File&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-        </div>
-         <input class="form-control" type="file" name="file" required/></td>
-      
-    </div> -->
-    <div class="row">
-        <div class="col-3">
-        </div>
-        <div class="col-md-auto"> 
-            <button type="button" class="btn btn-outline-primary" name="add"><i class="fas fa-plus"></i> 
-            Add 
-            </button>
-          
-        </div>
-        <div class="col-md-auto"> 
-            <button type="button" class="btn btn-outline-primary">
-            Edit
-            </button>
-        </div>
-        <div class="col-md-auto"> 
-            <button type="button" class="btn btn-outline-primary">
-            Delete
-            </button>
-        </div>
-        <div class="col-3">
-        </div>
-    </div>
-</div>
-</form>
+<div class="input-group mb-3">
 
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Upload  Your File and Link</label>
+		<input class="form-control" type="file" name="file" required/></td>
+</div>
+
+<h1 class="text-right">
+<hr>
+ <div class="row"> 
+
+ <div class="col-12">
+<button type="button" class="btn btn-primary btn-lg">Add</button>
+<button type="button" class="btn btn-secondary btn-lg">Edit</button>
+<button type="button" class="btn btn-secondary btn-lg">Delete</button>
+<button type="button" class="btn btn-outline-danger btn-lg"><a href="AddNotice.php">Back</a></button>
+</div>
+</div>
+<hr>
+  
  <!-- end your code here-->
 
 
