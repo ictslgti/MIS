3a4f1954c3c1815c82f7d66d80d97c717fca8ad1
achchuthany <achch1990@gmail.com>
2019-10-18 12:20:46 +0530

    DISPLAY NAME

--- controller/getModule.php
+++ controller/getModule.php
@@ -7,7 +7,7 @@ if(isset($_POST['course'])){
     $result = mysqli_query($con, $sql);
     if (mysqli_num_rows($result) > 0) {
         while($row = mysqli_fetch_assoc($result)) {
-        echo '<option  value="'.$row["module_id"].'" required>'.$row["module_name"].'</option>';
+        echo '<option  value="'.$row["module_id"].'" required>['.$id.'-'.$row["module_id"].'] '.$row["module_name"].'</option>';
         }
     }else{
         echo '<option value="null"   selected disabled>-- No Courses --</option>';
