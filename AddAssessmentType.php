deff2bf88e1652fd15d3ae4e3dd7d6e1e1079de2
Midhushan <mithunanirudh@gmail.com>
2019-10-18 10:39:15 +0530

    ok

--- AddAssessmentType.php
+++ AddAssessmentType.php
@@ -97,7 +97,7 @@ else{
                             </div>
                             <select
                                 class="custom-select<?php  if(isset($_POST['Add']) && empty($_POST['courser_id'])){echo ' is-invalid';}if(isset($_POST['Add']) && !empty($_POST['course_id'])){echo ' is-valid';} ?>"
-                                id="course" name="course_name" value="<?php echo $course_id; ?>"
+                                id="course_name" name="course_name" value="<?php echo $course_id; ?>"
                                 onchange="showModule(this.value)" required>
                                 <option value="null" selected disabled>--Select Course--</option>
 
@@ -138,9 +138,8 @@ else{
                                         class="fas fa-book-open"></i>&nbsp;&nbsp;Select Module&nbsp;</label>
                             </div>
                             <select
-                                class="custom-select<?php  if(isset($_POST['Add']) && empty($_POST['module_id'])){echo ' is-invalid';}if(isset($_POST['Add']) && !empty($_POST['module_id'])){echo ' is-valid';} ?>"
-                                id="Module" name="module_name" value="<?php echo $module_id; ?>" required>
-                                <option value="null" selected disabled>--Select Module--</option>
+                                class="custom-select" id="module_name" name="module_name" value="<?php echo $module_id; ?>" onchange="showModule(this.value)" required>
+                                <!-- // <option value="null" selected disabled>--Select Module--</option> -->
 
                             </select>
                         </div>
@@ -315,7 +314,7 @@ else{
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("Module").innerHTML = this.responseText;
+                document.getElementById("module_name").innerHTML = this.responseText;
             }
         };
         xmlhttp.open("POST", "controller/getModule", true);
