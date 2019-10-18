458f00d46ada2b311a64feb6fe5a3a4676c4cc98
nawodajanani <nawoda19961225@gmail.com>
2019-10-18 15:13:08 +0530

    index title changes

--- NoticeAddResult.php
+++ NoticeAddResult.php
@@ -10,14 +10,16 @@ include_once("menu.php");
 <?php
 //for add
 if(isset($_POST['Add'])){
-  if( !empty($_POST['result_id']) && !empty($_POST['Department']) && !empty($_POST['academic_year']) && !empty($_POST['Course']) && !empty($_POST['Module']))
+  if( !empty($_POST['result_id']) && !empty($_POST['department_id']) && !empty($_POST['academic_year']))
   {
      $result_id= $_POST['result_id'];
      $Department = $_POST['Department'];
      $AcademicYear = $_POST['AcademicYear'];
      $Course = $_POST['Course'];
      $Module = $_POST['Module'];
-     $sql = "INSERT INTO `notice_result`(`result_id`, `department_id`, `academic_year`,`course_id`,`module_id`) VALUES ('$result_id','$Department',`$AcademicYear`,'$Course',`$Module`,)";  
+     $sql = "INSERT INTO `notice_result`(`result_id`, `department_id`, `academic_year`,`course_id`,`module_id`)
+     
+      VALUES ('$result_id','$Department',`$AcademicYear`,'$Course',`$Module`,)";  
      if(mysqli_query($con,$sql))
      {
          echo "Record has been Inserted succesfully";
@@ -32,13 +34,10 @@ if(isset($_POST['Edit']))
 {  
    if(!empty($_POST['code'])&& !empty($_POST['name'])&& !empty($_GET['edit']))
    {   
-      $Department = $_POST['Department'];
-      $AcademicYear = $_POST['Academic Year'];
-      $Course = $_POST['Course'];
-      $Module = $_POST['Module'];
+      $code = $_POST['code'];
+      $name = $_POST['name'];
       $id = $_GET['edit'];
-      $sql = "UPDATE `notice_result` SET `result_id`='$result_id' ,`department_id`= '$Department',`academic_year`='$AcademicYear',`course_id`='$Course',`module_id`='$Module'
-      WHERE `notice_result`.`result_id`= $result_id";
+      $sql = "UPDATE `notice_result` SET `result_id`='$result_id' ,`department_id`= '$Department',`academic_year`='$AcademicYear',`course_id`='$Course',`module_id`'$Module'WHERE `department`.`department_id`= $id";
       if(mysqli_query($con,$sql))
       {
            echo"Record has been updated succesfully";
@@ -92,6 +91,29 @@ $Department=$Course=$Module=$AcademicYear=$Type=null;
   <h1>Add New Result</h1>
 </div>
 <hr>
+
+<form method="POST" action="#">
+  <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Result Id</label>
+    <div class="col-sm-10">
+	<select id="inputState" class="form-control<?php  if(isset($_POST['Add']) && empty($_POST['Department_id'])){echo ' is-invalid';}if(isset($_POST['Add']) && !empty($_POST['Department_id'])){echo ' is-valid';} ?>"  id="Department_id" name="Department_id">
+ 
+  <option selected disabled required>Result Id</option>
+  <?php          
+            $sql = "SELECT * FROM `department`";
+            $result = mysqli_query($con, $sql);
+            if (mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_assoc($result)) {
+                echo '<option  value="'.$row["department_id"].'" required';
+                if($row["department_id"]==$Department) echo ' selected';
+                echo '>'.$row["department_name"].'</option>';
+                }
+            }
+            ?>
+    </select>
+  </div>
+   </div>
+
 
 <form method="POST" action="#">
   <div class="form-group row">
