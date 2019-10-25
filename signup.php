b2d2fd9fe87a332cbfae9a9a595c45dacd2cfc96
HOMEPC-ACHCHUTH\ACHCHUTHAN <achch1990@gmail.com>
2019-10-25 20:38:45 +0530

    sign up file

---
+++ signup.php
@@ -0,0 +1,82 @@
+<!-- BLOCK#1 START DON'T CHANGE THE ORDER-->
+<?php
+$title = "Create your MIS @ SLGTI Account ";
+include_once("config.php");
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/signin.css">
+    <link href="css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <title><?php echo $title; ?></title>
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <div class="row no-gutter">
+            <!-- The image half -->
+            <div class="col-md-6 d-none d-md-flex bg-image"></div>
+
+
+            <!-- The content half -->
+            <div class="col-md-6 bg-light">
+                <div class="login d-flex align-items-center py-5">
+
+                    <!-- Demo content-->
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-10 col-xl-7 mx-auto">
+                                <h3 class="display-4 text-center">MIS@SLGTI</h3>
+                                <p class="text-muted text-center mb-4 blockquote-footer">Management Information System
+                                </p>
+                                <form  method="post">
+                                    <?php
+                                    if (!empty($msg))
+                                    echo '<div class="alert alert-danger rounded-pill border-0 shadow-sm px-4" >' . $msg . '</div>';
+                                    ?>
+                                    <div class="form-group mb-3">
+                                        <input id="inputEmail" type="text" name="username" placeholder="Username" required=""
+                                            autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4">
+                                    </div>
+                                    
+                                    <button type="submit" name="SignUp"
+                                        class="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm">Sign
+                                        up</button>
+
+                                    <div class="form-group mb-3 text-center">
+                                    <a href="passwordrecovery" class="font-italic text-muted pr-1">Forgot password?</a>
+                                   
+                                    <a href="signin"class="font-italic text-muted text-right">Sign in instead</a>
+                                    </div>
+                                    <div class="text-center d-flex justify-content-between mt-4">
+                                        <p>All Rights Reserved. Designed and Developed by Department of Information and
+                                            Communication Technology, <a href="http://slgti.com"
+                                                class="font-italic text-muted">
+                                                Sri Lanka-German Training Institute.</a></p>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div><!-- End -->
+
+                </div>
+            </div><!-- End -->
+
+        </div>
+    </div>
+</body>
+
+</html>
