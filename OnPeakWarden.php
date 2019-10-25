5b6618df89df4ca0c5ff7d979f02a3577e57fae9
nirthikaravienthiran <rnirthika3@gmail.com>
2019-10-25 11:45:52 +0530

    new dashboard for warden

---
+++ OnPeakWarden.php
@@ -0,0 +1,242 @@
+<!-- BLOCK#1 START DON'T CHANGE THE ORDER-->  
+<?php
+$title = "Home | SLGTI";
+ include_once("config.php"); 
+ include_once("head.php"); 
+ include_once("menu.php"); 
+ ?>
+<!--END DON'T CHANGE THE ORDER--> 
+
+<!--BLOCK#2 START YOUR CODE HERE -->
+   <!-- Content here -->
+
+   <br>     
+        <div class="row border border-light shadow p-3 mb-5 bg-white rounded">
+          <div class="col">
+          <br>
+            <blockquote class="blockquote text-center">
+                <h1 class="display-4">On peak</h1> 
+                <p class="mb-0">Department of Information and Communication Technology</p>
+                <footer class="blockquote-footer">Head of the Department<cite title="Source Title"></cite></footer>
+            </blockquote>
+          </div>
+        </div>
+
+
+
+
+<br>
+
+    <div class="border border-light shadow p-3 mb-5 bg-white rounded" > 
+      <div class="col">
+        <div class=row>
+            <div class="col">
+                <br>
+                <br>
+                <div id="timestamp" class="pr-5 pl-2 ml-auto text-info">9/18/2019 - 2:07:56 PM</div>
+                <br>
+                <br>
+            </div>
+        </div>
+        
+        
+
+      <div class=row >
+        <table class="table table-hover">
+            <thead>
+                  <tr>
+                    <th scope="col">NAME</th>
+                    <th scope="col">REGISTRATION NO </th>
+                    <th scope="col">REASON FOR EXIT</th>
+                    <th scope="col">EXIT DATE & TIME</th>
+                    <th scope="col">RETURN DATE & TIME</th>
+                    <th scope="col"> CONTACT NO </th>
+                    <th scope="col">ACTION</th>
+                    
+                  </tr>
+            </thead>
+            <tbody>
+                  <tr>
+                    <th scope="row">Nirthika Ravinthiran</th>
+                    <td> 24586</td>
+                    <td> Hospital </td>
+                    <td> 25.2.2019/ 10.00 am</td>
+                    <td> 25.2.2019/ 1.00 pm</td>
+                    <td> 077896542</td>
+                    <td>
+                        <button type="button" class="btn btn-outline-primary">Approved</button>
+                        <button type="button" class="btn btn-outline-danger">Disapproved</button>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row">Piruntha Jeyananthan</th>
+                    <td> 24586</td>
+                    <td> Other Reason </td>
+                    <td>20.6.2019/ 9.00 am</td>
+                    <td> 20.6.2019/ 2.00 pm</td>
+                    <td> 077856542</td>
+                    <td>
+                        <button type="button" class="btn btn-outline-primary">Approved</button>
+                        <button type="button" class="btn btn-outline-danger">Disapproved</button>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row">Hephzibha Pathmanathan</th>
+                    <td> 24586</td>
+                    <td> Family issues</td>
+                    <td>21.7.2019/ 10.30 am</td>
+                    <td> 21.7.2019/ 12.00 noon</td>
+                    <td> 077456542</td>
+                    <td>
+                        <button type="button" class="btn btn-outline-primary">Approved</button>
+                        <button type="button" class="btn btn-outline-danger">Disapproved</button>
+                    </td>
+                  </tr>
+            </tbody>
+        </table> 
+      </div>
+    </div>
+  </div>
+ 
+
+  <div class="border border-light shadow p-3 mb-5 bg-white rounded" > 
+      <div class="col">
+        <div class=row>
+            <div class="col">
+                <br>
+                <br>
+                 <nav class="navbar navbar-light bg-light">
+                        <form class="form-inline">
+                        <div class="pr-5 pl-2 ml-auto text-info">History</div>
+
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <input class="form-control mr-sm-2" type="search" placeholder="Registration No" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i> </button>
+
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <input class="form-control mr-sm-2" type="search" placeholder="Date" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+                        
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <input class="form-control mr-sm-2" type="search" placeholder="Reference" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+                        </form>
+                </nav>
+                <br>
+                <br>
+            </div>
+        </div>
+        
+        
+
+      <div class=row >
+        <table class="table">
+            <thead>
+                  <tr>
+                    <th scope="col">NAME</th>
+                    <th scope="col">REGISTRATION NO </th>
+                    <th scope="col">REASON FOR EXIT</th>
+                    <th scope="col">EXIT DATE & TIME</th>
+                    <th scope="col">RETURN DATE & TIME</th>
+                    <th scope="col"> CONTACT NO </th>
+                    <th scope="col">REFERENCE</th>
+                    
+                  </tr>
+            </thead>
+            <tbody>
+                  <tr>
+                    <th scope="row">Nirthika Ravinthiran</th>
+                    <td> 24586</td>
+                    <td> Hospital </td>
+                    <td> 25.2.2019/ 10.00 am</td>
+                    <td> 25.2.2019/ 1.00 pm</td>
+                    <td> 077896542</td>
+                    <td> approved </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row">Piruntha Jeyananthan</th>
+                    <td> 24586</td>
+                    <td> Other Reason </td>
+                    <td>20.6.2019/ 9.00 am</td>
+                    <td> 20.6.2019/ 2.00 pm</td>
+                    <td> 077856542</td>
+                    <td> disapproved</td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row">Hephzibha Pathmanathan</th>
+                    <td> 24586</td>
+                    <td> Family issues</td>
+                    <td>21.7.2019/ 10.30 am</td>
+                    <td> 21.7.2019/ 12.00 noon</td>
+                    <td> 077456542</td>
+                    <td> approved </td>
+                  </tr>
+            </tbody>
+        </table> 
+      </div>
+    </div>
+  </div>
+    
+
+<br>
+<br>
+
+
+
+       <div class="row ">
+          <div class="col-3 ">
+          <div class="card shadow-sm p-3 mb-5 bg-white rounded" style="width: 18rem;">
+              <div class="card-body">
+              <h5 class="card-title">Leave of Absence </h5>
+              <p class="card-text">A LOA is an extended period of time off from their studies. 
+                    there may be a formal process you need to follow to get approved for a leave.</p>
+              </div>
+              </div>
+          </div>
+
+          <div class="col-3 ">
+              <div class="card shadow-sm p-3 mb-5 bg-white rounded" style="width: 18rem;">
+              <div class="card-body">
+              <h5 class="card-title">Time Schedule</h5>
+              <p class="card-text">This form must be submitted to the guards, when students wants to exit SLGTI during scgool hours/ on peak (8.15 am- 4.15 pm)</p>
+              </div>
+              </div>
+          </div>
+
+          <div class="col-3">
+              <div class="card shadow-sm p-3 mb-5 bg-white rounded" style="width: 18rem;">
+              <div class="card-body">
+              <h5 class="card-title">Jurisdiction of the Code</h5>
+              <p class="card-text">Please note that students fail within the jurisdiction of the code of conduct and honor for off-campus conduct.</p>
+              </div>
+              </div>
+          </div>
+
+          <div class="col-3">
+              <div class="card shadow-sm p-3 mb-5 bg-white rounded" style="width: 18rem;">
+              <div class="card-body">
+              <h5 class="card-title">Approvel</h5>
+              <p class="card-text">Please supervise the reason for students temporary exit in the box below, state the date , inform the warden. </p>
+              </div>
+              </div>
+          </div>
+      </div>
+
+ 
+ 
+
+<!--END OF YOUR COD-->
+
+<!--BLOCK#3 START DON'T CHANGE THE ORDER-->   
+<?php include_once("footer.php"); ?>
+<!--END DON'T CHANGE THE ORDER-->  
