8579785aace229619872b8b4ec91ca3cecc6c498
nirthikaravienthiran <rnirthika3@gmail.com>
2019-10-25 11:38:51 +0530

    RENAMED

---
+++ RequestOnPeak.php
@@ -0,0 +1,219 @@
+<!-- BLOCK#1 START DON'T CHANGE THE ORDER-->  
+<?php
+$title = "Home | SLGTI";
+ include_once("config.php"); 
+ include_once("head.php"); 
+ include_once("menu.php"); 
+ ?>
+<!--END DON'T CHANGE THE ORDER--> 
+
+<!--BLOCK#2 START YOUR CODE HERE -->
+<br>
+<div class="row border border-light shadow p-3 mb-5 bg-white rounded">
+          <div class="col">
+          <br>
+          <br>
+            <blockquote class="blockquote text-center">
+                <h1 class="display-4">On peak Request</h1> 
+                <p class="mb-0">Department of Information and Communication Technology</p>
+                <footer class="blockquote-footer">Temporary Exit Application<cite title="Source Title"></cite></footer>
+            </blockquote>
+          </div>
+</div>
+
+<!-- card start here-->
+
+<div class="border border-light shadow p-3 mb-5 bg-white rounded" > 
+<br>    
+<div class="table container">    
+    <div class="container">
+        <div class="intro">
+
+        <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label class="input-group-text" for="inputGroupSelect01"> 
+                            <i class="fas fa-user"> </i>&nbsp;&nbsp;Full Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                    </div>
+                    <input class="form-control" type="text" placeholder="Nirthika Ravienthiran">
+        </div>
+
+<br>
+
+        <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label class="input-group-text" for="inputGroupSelect01"> 
+                            <i class="fas fa-fingerprint"> </i>&nbsp;&nbsp;Registration No&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                    </div>
+                    <input class="form-control" type="text" placeholder="2018/SLGTI/5IT/04">
+        </div>
+
+<br>       
+
+        <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label class="input-group-text" for="inputGroupSelect01">
+                            <i class="fas fa-school"></i>&nbsp;&nbsp;Department&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                    </div>
+                    <select class="custom-select" id="inputGroupSelect01">
+                        <option selected>Choose...</option>
+                        <option value="1">Department of Information & Communication Technology </option>
+                        <option value="1">Department of Food Technology </option>
+                        <option value="1">Department of Mechnical Technology </option>
+                        <option value="1">Department of Automobile Technology </option>
+                        <option value="1">Department of Construction Technology </option>
+                        <option value="1">Department of Electrical Technology </option>
+                    </select>
+        </div>
+
+<br>
+
+        <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label class="input-group-text" for="inputGroupSelect01"> 
+                            <i class="fas fa-phone-alt"> </i>&nbsp;&nbsp;Contact no&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                    </div>
+                    <input class="form-control" type="text" placeholder="Mobile or Home number">
+        </div>
+
+<br>
+
+        <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label class="input-group-text" for="inputGroupSelect01">
+                            <i class="fas fa-sign-out-alt"></i>&nbsp;&nbsp;Reason for Exit&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                    </div>
+                    <select class="custom-select" id="inputGroupSelect01">
+                        <option selected>Choose...</option>
+                        <option value="1">Hospital </option>
+                        <option value="1">Family issues </option>
+                        <option value="1">Other Reasons</option>
+                    
+                    </select>
+        </div>
+
+<br>
+
+        <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label class="input-group-text" for="inputGroupSelect01"> 
+                            <i class="far fa-clock"> </i>&nbsp;&nbsp;Exit Date and Time&nbsp;</label>
+                    </div>
+                    <input class="form-control" type="text" placeholder="2019.02.06 - 10.00 pm">
+        </div>
+
+<br>
+
+        <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label class="input-group-text" for="inputGroupSelect01"> 
+                            <i class="fas fa-history"> </i>&nbsp;&nbsp;Return Time&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                    </div>
+                    <input class="form-control" type="text" placeholder="12.00 noon">
+        </div>
+
+<br>
+
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Comments : </label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+
+<br>
+        
+        <div class=row>
+            <div class="col">
+                <blockquote class="blockquote text-center">
+                    <p class="mb-0">I have read and understand the terms and conditions. I have agreed by the abide by the rules and regulations of SLGTI.</p>
+                </blockquote>
+            </div>
+        </div>
+
+<br>
+      
+        <div class="row">
+            <div class="col">
+                <div class="mx-auto" style="width: 200px;">
+                <button type="button" class="btn btn-primary "><i class="fab fa-telegram"></i>&nbsp;&nbsp;&nbsp;Request to approval</button>
+                   
+                </div>
+            </div>
+        </div>
+       
+       
+               
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="border border-light shadow p-3 mb-5 bg-white rounded" >
+    <div class="col">
+        <blockquote class="blockquote text-center">
+            <p class="mb-0">Terms and Conditions of SLGTI </p>
+            <footer class="blockquote-footer">This request must be approved by the HOD and Warden, when students want to exit SLGTI during school hours/ on peak (8.15 am - 4.15 pm)  <cite title="Source Title"></cite></footer>
+            <footer class="blockquote-footer">Please note that you fail with in the jurisdiction of the code of conduct and honor for off-campus conduct. <cite title="Source Title"></cite></footer>
+            <footer class="blockquote-footer">Please indicate the reason for your temporary exit in the box above, state the date and seek for approval by your HEAD of the Department (HoD). <cite title="Source Title"></cite></footer>
+        </blockquote> 
+    </div>
+</div>
+
+
+
+<div class="border border-light shadow p-3 mb-5 bg-white rounded" > 
+      <div class="col">
+        <div class=row>
+            <div class="col">
+                <br>
+                <br>
+                <div class="pr-5 pl-2 ml-auto text-info">History</div>
+                <br>
+                <br>
+            </div>
+        </div>
+        
+        
+
+      <div class=row >
+        <table class="table">
+            <thead>
+                  <tr>
+                    <th scope="col">EXIT DATE & TIME</th>
+                    <th scope="col">RETURN DATE & TIME</th>
+                    <th scope="col">REASON FOR EXIT</th>
+                    <th scope="col">REFERENCE</th>
+                    </tr>
+            </thead>
+            <tbody>
+                  <tr>
+                    <td> 25.2.2019/ 10.00 am</td>
+                    <td> 25.2.2019/ 1.00 pm</td>
+                    <td> Hospital </td>
+                    <td> approved </td>
+                  </tr>
+
+                  <tr>
+                    <td>20.6.2019/ 9.00 am</td>
+                    <td> 20.6.2019/ 2.00 pm</td>
+                    <td> Other Reason </td>
+                    <td> disapproved</td>
+                  </tr>
+
+                  <tr>
+                    <td>21.7.2019/ 10.30 am</td>
+                    <td> 21.7.2019/ 12.00 noon</td>
+                    <td> Family issues</td>
+                    <td> approved </td>
+                  </tr>
+            </tbody>
+        </table> 
+      </div>
+    </div>
+  </div>
+    
+
+<!--END OF YOUR COD-->
+
+<!--BLOCK#3 START DON'T CHANGE THE ORDER-->   
+<?php include_once("footer.php"); ?>
+<!--END DON'T CHANGE THE ORDER-->  
