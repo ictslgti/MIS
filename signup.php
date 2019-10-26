339b768e6457ca760301f278fc4ad62659293a53
HOMEPC-ACHCHUTH\ACHCHUTHAN <achch1990@gmail.com>
2019-10-26 13:03:17 +0530

    user activation by email verfication

--- signup.php
+++ signup.php
@@ -2,9 +2,103 @@
 <?php
 $title = "Create your MIS @ SLGTI Account ";
 include_once("config.php");
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;         
+require './library/PHPMailer/autoload.php';
+$msg =$msgs = null;
+if (isset($_POST['SignUp']) && !empty($_POST['username'])) {
+  $user_name = htmlspecialchars($_POST['username']);
 
+  $temporary_timestamp = time();
+
+  $user_activation_hash = sha1(uniqid(mt_rand(), true));
+
+  $sql = "SELECT user_email,user_active FROM user WHERE user_name = '$user_name' ";
+  $result = mysqli_query($con,$sql);
+  if(mysqli_num_rows($result)==1){
+    $row = mysqli_fetch_assoc($result);
+    $user_email = $row['user_email'];
+    $user_active = $row['user_active'];
+    if($user_active==0){
+        $sql = "UPDATE user SET user_activation_hash = '$user_activation_hash',
+                user_password_reset_timestamp = '$temporary_timestamp'
+                WHERE user_name = '$user_name'";
+        if(mysqli_query($con,$sql)){
+            if(EMAIL_USE_SMTP){
+                $mail = new PHPMailer;
+                $mail->isSMTP();
+                $mail->Host = EMAIL_SMTP_HOST;
+                $mail->Port = 587;
+                $mail->SMTPAuth = true;
+                $mail->Username = EMAIL_SMTP_USERNAME;
+                $mail->Password = EMAIL_SMTP_PASSWORD;
+                $mail->setFrom(EMAIL_VERIFICATION_FROM, EMAIL_VERIFICATION_FROM_NAME);
+                $mail->addReplyTo(EMAIL_VERIFICATION_FROM, EMAIL_VERIFICATION_FROM_NAME);
+                $mail->addAddress($user_email);
+                $mail->Subject = EMAIL_VERIFICATION_SUBJECT;
+                $link    = EMAIL_VERIFICATION_URL.'?un='.urlencode($user_name).'&vc='.urlencode($user_activation_hash);
+                $html_message = "
+                    Dear  $user_name,
+                    <br />
+                    We received a request for activate your <b> $user_name </b> account at ". EMAIL_PASSWORDRESET_FROM_NAME.".
+                    <br />
+                    Go to this page $link  to set your password to activate your account.  <br />
+                    <i> The link will be active for one hour. </i>
+                    <br />
+                    <br />
+                    Best Regards,
+                    <br />
+                    The ICT Support Team
+                    <br />   
+                    <i> Technical Support Contact: achchuthan@slgti.com </i>
+                ";
+                $mail->msgHTML($html_message, __DIR__);
+                if (!$mail->send()) {
+                    $msg = 'Password mail not sent';
+                } else {
+                    $msgs = 'Password mail sent';
+                }
+            }   
+        }else{
+            $msg = 'Database error';
+        }
+
+
+    }else{
+        $msg = 'User is Active. Use <a href="passwordrecovery"> Forgot password? <a/a> option to reset your password';
+    }
+  }else{
+    $msg = 'User not exist';
+  }
+
+}
 ?>
-
+<?php
+if (isset($_POST['ChangePassword']) && !empty($_POST['password'])&& !empty($_POST['repeatpassword'])) {
+    $password = htmlspecialchars($_POST['password']);
+    $repeatpassword = htmlspecialchars($_POST['repeatpassword']);
+    $user_name = htmlspecialchars($_GET['un']);
+    $user_activation_hash = htmlspecialchars($_GET['vc']);
+    if($password==$repeatpassword){
+        if(strlen($password)>=8){
+            $password_hash = hash('sha256', $password);
+            $sql_r = "UPDATE user  SET user_password_hash = '$password_hash', user_active = 1,
+            user_activation_hash = NULL, user_password_reset_timestamp = NULL
+            WHERE user_name = '$user_name' AND user_activation_hash = '$user_activation_hash'";
+             if(mysqli_query($con,$sql_r)){
+                $msgs = 'User Activated';
+             }else{
+                $msg = 'User Activation failed';
+             }
+             
+        }else{
+            $msg = 'Password too short';
+        }
+    }else{
+        $msg = 'Bad confirm password';
+    }
+}
+?>
 
 <!doctype html>
 <html lang="en">
@@ -51,6 +145,9 @@ include_once("config.php");
                                     if (!empty($msgs))
                                     echo '<div class="alert alert-success rounded-pill border-0 shadow-sm px-4" >' . $msgs . '</div>';
                                     ?>
+                                    <?php 
+                                    if(!isset($_GET['un']) && !isset($_GET['vc'])){
+                                    ?>
                                     <div class="form-group mb-3">
                                         <input id="inputEmail" type="text" name="username" placeholder="Username" required=""
                                             autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4">
@@ -59,7 +156,34 @@ include_once("config.php");
                                     <button type="submit" name="SignUp"
                                         class="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm">Sign
                                         up</button>
+                                    <?php
+                                    }
+                                    ?>
+                                    <?php 
+                                    if(isset($_GET['un']) && isset($_GET['vc'])){
+                                    ?>
+                                    <!-- reset password? -->
+                                    <div class="form-group mb-3">
+                                        <input id="inputpassword" type="password" name="password" placeholder="New password" required=""
+                                            autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4" onkeyup="checkPass(); return false;">
+                                    </div>
 
+                                    <div class="form-group mb-3">
+                                        <input id="inputrepeatpassword" type="password" name="repeatpassword" placeholder="Re-enter new password" required=""
+                                            autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4" onkeyup="checkPass(); return false;" >
+                                    </div> 
+
+                                    <div class="d-flex mt-4">
+                                    <p class="text-center" id="error-nwl"></p>
+                                    </div>
+                                    <button type="submit" name="ChangePassword" id="ChangePassword"
+                                        class="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm">Activate  Account</button>
+                                    
+                                    <!-- reset password? -->
+
+                                    <?php
+                                    }
+                                    ?>
                                     <div class="form-group mb-3 text-center">
                                     <a href="passwordrecovery" class="font-italic text-muted pr-1">Forgot password?</a>
                                    
@@ -81,6 +205,44 @@ include_once("config.php");
 
         </div>
     </div>
+    <script>
+    document.getElementById("ChangePassword").disabled = true;
+    function checkPass(){
+    var pass1 = document.getElementById('inputpassword');
+    var pass2 = document.getElementById('inputrepeatpassword');
+    var message = document.getElementById('error-nwl');
+    var goodColor = "rgb(147, 255, 171)";
+    var badColor = "rgb(255, 201, 206)";
+ 	
+    if(pass1.value.length >= 8)
+    {
+        pass1.style.backgroundColor = goodColor;
+        message.style.color = goodColor;
+        message.innerHTML ="";
+    }
+    else
+    {
+        pass1.style.backgroundColor = badColor;
+        message.style.color = badColor;
+        message.innerHTML = " You have to enter at least 8 digit!"
+        return;
+    }
+  
+    if(pass1.value == pass2.value)
+    {
+        pass2.style.backgroundColor = goodColor;
+        message.style.color = goodColor;
+        document.getElementById("ChangePassword").disabled = false;
+        message.innerHTML ="";
+    }
+	else
+    {
+        pass2.style.backgroundColor = badColor;
+        message.style.color = badColor;
+        message.innerHTML = " These passwords don't match"
+    }
+}  
+</script>
 </body>
 
 </html>
