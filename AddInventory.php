ce8302ecd3568f237b3e38c58f52f686b13c6193
saruyansaru02 <saruyansaru02@gmail.com>
2019-11-01 13:21:16 +0530

    index title changes“

--- AddInventory.php
+++ AddInventory.php
@@ -3,6 +3,7 @@ $title = "Department Details | SLGTI";
  include_once("config.php"); 
  include_once("head.php"); 
  include_once("menu.php"); 
+ if($_SESSION['user_type']!='STU'){ 
  ?>
 <!-- end default code -->
 
@@ -258,7 +259,7 @@ if(isset($_GET['edits'])){
 
 
 <!-- end my code\ -->
-
+ <?php }?>
 
 <!--BLOCK#3 START DON'T CHANGE THE ORDER-->
 <?php include_once("footer.php"); ?>
