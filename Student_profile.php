db7c25b51b505e9b28da9a07aaa645085a299120
thanujahthanujah <thanujah1996@gmail.com>
2019-11-02 12:19:09 +0530

    profile

--- Student_profile.php
+++ Student_profile.php
@@ -17,6 +17,54 @@ include_once("menu.php");
 
 
 <!-----END YOUR CODE----->
+<?php
+$stid = $title = $fname = $ininame = $gender = $civil = $email = $nic = $dob = $phone = $address = $zip = $district = $division = $province = $blood = $mode =
+$ename = $eaddress = $ephone = $erelation = $enstatus = $coid = $year = $enroll = $exit = $qutype = $index = $yoe = $subject = $results = null;
+
+$username = $_SESSION['user_name'];
+
+$sql = "SELECT user_name,`student_title`,`student_fullname`,`student_ininame`,`student_gender`,`student_civil`,`student_email`,`student_nic`,`student_profile_img`,
+`student_dob`,`student_phone`,`student_address`,`student_zip`,`student_district`,`student_divisions`,`student_provice`,`student_blood`,`student_em_name`,`student_em_address`,
+`student_em_phone`,`student_em_relation`,`student_status`,`course_name`,`department_name`,`course_mode`,course_nvq_level,`academic_year`,`student_enroll_date`,`student_enroll_exit_date`,
+`student_enroll_status` FROM `student` as s, student_enroll as e, user as u, course as c, department as d WHERE user_name=s.student_id and s.student_id=e.student_id 
+ and e.course_id=c.course_id and  c.department_id=d.department_id and  user_name='$username'";
+$result = mysqli_query($con,$sql);
+
+  if(mysqli_num_rows($result)==1)
+  {
+    echo "success";
+    $row =mysqli_fetch_assoc($result);
+    //$stid = $row['student_id'];
+    $title = $row['student_title'];
+    $fname = $row['student_fullname'];
+    $ininame = $row['student_ininame'];
+    $gender = $row['student_gender'];
+    $civil = $row['student_civil'];
+    $email = $row['student_email'];
+    $nic = $row['student_nic'];
+    $dob = $row['student_dob'];
+    $phone = $row['student_phone'];
+    $address = $row['student_address'];
+    $zip = $row['student_zip'];
+    $district = $row['student_district'];
+    $division = $row['student_divisions'];
+    $province = $row['student_provice'];
+    $blood = $row['student_blood'];
+    $ename = $row['student_em_name'];
+    $eaddress = $row['student_em_address'];
+    $ephone = $row['student_em_phone'];
+    $erelation = $row['student_em_relation'];
+    $coid = $row['course_name'];
+    $depth = $row['department_name'];
+    $level = $row['course_nvq_level'];
+    $mode = $row['course_mode'];
+    $year = $row['academic_year'];
+    $enstatus =$row['student_enroll_status'];
+    $enroll = $row['student_enroll_date'];
+    $exit = $row['student_enroll_exit_date'];
+  }
+
+?>
 <!-- form start---->
 <div class="col text-center shadow p-5 mb-5 bg-white rounded">
 <h1 style="text-align:center"  > SRI LANKA GERMAN TRAINING INSTITUTE  </h1>
@@ -32,13 +80,77 @@ include_once("menu.php");
     <!-- <button type="button" class="btn btn-outline-success">Success</button> -->
     </div>
     <div class="col-md-7 col-sm-4">
-        <h5 class="text-muted"><b>Miss.Ravinthiran.Thanujah</b></h5>
-        <h6 class="text-muted">2017/ICT/BIT-06</h6>
-        <h6 class="text-muted">NIC: 966122145v</h6>
-        <h6 class="text-muted"> Department of Information & Communication Technology</h6>
-        <h6 class="text-muted"> National Diploma in Information & Communication Technology</h6>
-        <p class="text-muted" style="font-size:15px;"> (NVQ-05 Full Time) </P>
-        <h6 class="text-muted">Batch: 2017/2018 (1ST NOVEMBER 2018)</h6>
+        <h5 class="text-muted"><b><?php echo $title.".".$fname; ?></b></h5>
+        <h6 class="text-muted"><?php echo $username; ?></h6>
+        <h6 class="text-muted"><?php echo $nic; ?></h6>
+        <h6 class="text-muted">
+        <?php 
+        $sql="select d.department_name from department as d, course as c, student_enroll as e, user as u where user_name=e.student_id and 
+        e.course_id=c.course_id and student_enroll_status='Following' and c.department_id=d.department_id and user_name='$username'";
+        $result = mysqli_query($con,$sql);
+
+        if(mysqli_num_rows($result)==1)
+        {
+        echo "Department of ".$depth; 
+        }
+        else
+        {
+            echo "Course has been Completed";
+        }
+
+        ?>
+        </h6>
+        <h6 class="text-muted">
+        <?php 
+        $sql="select c.course_name from  course as c, student_enroll as e, user as u where user_name=e.student_id and 
+        e.course_id=c.course_id and student_enroll_status='Following' and user_name='$username'";
+        $result = mysqli_query($con,$sql);
+
+        if(mysqli_num_rows($result)==1)
+        {
+        echo "Department of ".$coid; 
+        }
+        else
+        {
+            echo "Course has been Completed";
+        }
+
+        ?>
+        </h6>
+        <p class="text-muted" style="font-size:15px;">
+        <?php 
+        $sql="select c.course_nvq_level from course as c, student_enroll as e, user as u where user_name=e.student_id and 
+        e.course_id=c.course_id and student_enroll_status='Following' and user_name='$username'";
+        $result = mysqli_query($con,$sql);
+
+        if(mysqli_num_rows($result)==1)
+        {
+        echo "Department of ".$level; 
+        }
+        // else
+        // {
+        //     echo "Course has been Completed";
+        // }
+
+        ?>
+        </P>
+        <h6 class="text-muted">
+        <?php 
+        $sql="select DISTINCT max(academic_year),`student_enroll_exit_date` from student_enroll WHERE student_id='$username' ORDER BY academic_year DESC";
+        
+        $result = mysqli_query($con,$sql);
+
+        if(mysqli_num_rows($result)==1)
+        {
+        echo "Batch: ".$year."( ".$exit." )"; 
+        }
+        // else
+        // {
+        //     echo "Course has been Completed";
+        // }
+
+        ?>
+        </h6>
         <!-- <div class="form-row">
         <div class="col-md-5 col-sm-4"></div>
         <div class="col-md-5 col-sm-4"><h5 class="text-muted" style="flot:left">2018 November 01</h5></div> 
@@ -59,7 +171,7 @@ include_once("menu.php");
   <div class="nav nav-tabs shadow p-2 mb-4 bg-white rounded" id="nav-tab" role="tablist">
     <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Personal Info</a>
     <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Password</a>
-    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Mosules Info</a>
+    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Qualification Info</a>
   </div>
 </nav>
 <div class="tab-content shadow p-2 mb-4 bg-white rounded" id="nav-tabContent">
@@ -220,7 +332,7 @@ include_once("menu.php");
                 <input type="reset" class="btn btn-light" value="Cancel">
             </div>
         </div>
-        <!-- </form>        -->
+        <!-- </form>-->
   </div>
 
   <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
@@ -234,18 +346,17 @@ include_once("menu.php");
               <th width="15%"> Year of Exam </th>
               <th width="30%"> Subject </th>
               <th width="10%"> Result </th>
-              <th width="10%"> Action </th>
               </tr>
               </head>
               <tbody>
               <?php
-               if(isset($_GET['edit']))
-               {
+            //    if(isset($_GET['edit']))
+            //    {
                   //$stid =$_GET['edit'];WHERE `qualification_student_id`= '$stid'"
                   //include_once("mysqli_connect.php");
-                  $stid =$_GET['edit'];
-                  $sql ="SELECT `qualification_type`,`qualification_index_no`,`qualification_year`,`qualification_description`,`qualification_results` 
-                  FROM `student_qualification` where `qualification_student_id`= '$stid' ";
+                  $username = $_SESSION['user_name'];
+                  $sql ="SELECT`qualification_type`,`qualification_index_no`,`qualification_year`,`qualification_description`,`qualification_results`
+                  FROM `user`,student_qualification WHERE `user_name`= qualification_student_id and `user_name`='$username'";
                   $result = mysqli_query ($con, $sql);
                   if (mysqli_num_rows($result)>0)
                   {
@@ -258,9 +369,6 @@ include_once("menu.php");
                           <td>'. $row["qualification_year"].'</td>
                           <td>'. $row["qualification_description"].'</td>
                           <td>'. $row["qualification_results"].'</td>
-                          <td>
-                          <a href="?DELETE='.$stid.'&quali='.$row["qualification_description"].'&deinde='.$row["qualification_index_no"].'"class="btn btn-outline-danger btn-icon-split"""> <i class="far fa-trash-alt"></i> &nbsp;&nbsp;</a>
-                          </td>
                       </tr> ';
                     }
                   }
@@ -268,7 +376,6 @@ include_once("menu.php");
                   {
                     echo "0 results";
                   }
-               }
 
                ?>
               </tbody>
