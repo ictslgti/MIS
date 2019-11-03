1650813afa7ececb31288c98c93b3fddd8916d13
Jayanth <54566785+marian8863@users.noreply.github.com>
2019-11-03 14:33:08 +0530

    Update feedbackForm.php

--- feedbackForm.php
+++ feedbackForm.php
@@ -24,19 +24,10 @@ $sql="SELECT  `feedback_survey`.`survey_id` AS `survey_id`,`course`.`course_name
         $s_dept=$row["course_name"];
         $s_module= $row["module_name"];
         $s_edate=$row["end_date"];
-        
-   
-<<<<<<< HEAD
     }else{
         echo "Error :-".$sql.
         "<br>"  .mysqli_error($con);
-=======
-    }else {
-       echo "Error :-".$sql.
-              "<br>"  .mysqli_error($con);
->>>>>>> 882b2a89e5d54b62fd0d373dc09fd782fa8a5772
-    }
-
+ 
 ?>
 <!--END DON'T CHANGE THE ORDER-->
 
