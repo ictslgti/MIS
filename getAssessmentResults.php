d2dbe445bb2cc9fb9efed64b55d6bbed2ff32eea
Midhushan <39880516+Midhushan@users.noreply.github.com>
2019-11-03 10:35:40 -0800

    Update getAssessmentResults.php
    done

--- getAssessmentResults.php
+++ getAssessmentResults.php
@@ -7,10 +7,10 @@
                     $postchar = (int)$_POST['id'];
                     //multiply the id value by 10 to set SQL OFFSET value
                     $postchar = $postchar*10;                 
-                     $query ="SELECT assessments_marks.assessment_marks_id,assessments_marks.assessment_id,assessments_marks.student_id,assessments_marks.assessment_attempt,assessments_marks.assessment_marks,assessments_marks.assessment_marks_grade FROM assessments_marks ASC LIMIT 10 OFFSET $postchar";
+                     $query ="SELECT assessments_marks.assessment_marks_id,assessments_marks.module_id,assessments_marks.assessment_id,assessments_marks.student_id,assessments_marks.assessment_attempt,assessments_marks.assessment_marks,assessments_marks.assessment_marks_grade FROM assessments_marks ASC LIMIT 10 OFFSET $postchar";
                 }
                else{ 
-                    $query = "SELECT assessment_marks_id,assessment_id,student_id,assessment_attempt,assessment_marks,assessment_marks_grade FROM assessments_marks LIMIT 10 OFFSET 0"; 
+                    $query = "SELECT assessment_marks_id,module_id,assessment_id,student_id,assessment_attempt,assessment_marks,assessment_marks_grade FROM assessments_marks LIMIT 10 OFFSET 0"; 
                 }  
                $result = mysqli_query($con, $query);
             //    if (!$check1_res) {
@@ -21,6 +21,7 @@
                     echo '  
                          <tr>  
                             <td>'.$row['assessment_marks_id'].'</td>
+                            <td>'.$row['module_id'].'</td>
                             <td>'.$row['assessment_id'].'</td>
                             <td>'.$row['student_id'].'</td>
                             <td>'.$row['assessment_attempt'].'</td>
