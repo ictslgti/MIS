56eaa5989c6c320a6c067cea3e9e07aab525a7d5
jureesun <54789051+jureesun@users.noreply.github.com>
2019-11-03 09:45:00 +0530

    Update Payment.php

--- Payment.php
+++ Payment.php
@@ -153,18 +153,18 @@ if(isset($_POST['edit'])){
                         <label for="inputEmail4">ID</label>
                         <input type="text" name="student_id" value="<?php echo  $student_id;?>"
                             class="form-control<?php  if(isset($_POST['Add']) && empty($_POST['student_id'])){echo ' is-invalid';}if(isset($_POST['Add']) && !empty($_POST['student_id'])){echo ' is-valid';} ?>"
-                            id="inputEmail4" placeholder="ID">
+                            id="inputEmail4" placeholder="ID" disabled>
                     </div>
                     <div class="form-group col-md-12"><i class="fas fa-user"></i>&nbsp;
                         <label for="inputEmail4">Name</label>
                         <input type="text" value="<?php echo  $student_name; ?>" name="student_name"
-                            class="form-control <?php  if(isset($_POST['Add']) && empty($_POST['student_name'])){echo ' is-invalid';}if(isset($_POST['Add']) && !empty($_POST['student_name'])){echo ' is-valid';} ?>" id="inputEmail4" placeholder="Name">
+                            class="form-control <?php  if(isset($_POST['Add']) && empty($_POST['student_name'])){echo ' is-invalid';}if(isset($_POST['Add']) && !empty($_POST['student_name'])){echo ' is-valid';} ?>" id="inputEmail4" placeholder="Name" disabled>
                     </div>
                     <div class="form-group col-md-12"><i class="fas fa-building"></i>&nbsp;
                         <label for="inputEmail4">Department</label>
                         <input type="Department" 
                             class="form-control <?php  if(isset($_POST['Add']) && empty($_POST['pays_department'])){echo ' is-invalid';}if(isset($_POST['Add']) && !empty($_POST['pays_department'])){echo ' is-valid';} ?>"" id="
-                            inputEmail4" placeholder="Department" name="pays_department" value="<?php echo  $department;?>">
+                            inputEmail4" placeholder="Department"  name="pays_department disabled" value="<?php echo  $department;?>">
                     </div>
                 </div>
 
