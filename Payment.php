4664f8b9661285352dd62a9322d688bd3659585b
jureesun <54789051+jureesun@users.noreply.github.com>
2022-06-20 12:06:58 +0530

    Update Payment.php
    admin

--- Payment.php
+++ Payment.php
@@ -3,7 +3,7 @@ $title="payment |SLGTI";
 include_once("config.php");
 include_once("head.php");
 include_once("menu.php");
- if($_SESSION['user_type']=='ACC'){ 
+ if($_SESSION['user_type']=='ACC'||'ADM'){ 
 ?>
 <!-- dont change -->
 
